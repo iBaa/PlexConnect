fa44527b71262c9b0d5e18ef8f0db1503c626bd1
iBaa <bernhard.baaser@gmx.de>
2013-04-22 21:57:26 +0200

    XML generation rewrite: now based on XML templates with special keywords

--- XMLConverter.py
+++ XMLConverter.py
@@ -19,6 +19,7 @@ http://trailers.apple.com/appletv/us/nav.xml
 import sys
 import inspect 
 import string, cgi, time
+import copy  # deepcopy()
 from os import curdir, sep
 import httplib, socket
 
@@ -125,226 +126,6 @@ def XML_ReadFromURL(address, path):
 
 
 
-def XML_Directory(elem, PMS_XML, path):
-    PMSroot = PMS_XML.getroot()
-    
-    el = etree.SubElement(elem, "body")
-    el = etree.SubElement(el, "listScrollerSplit", {'id':"com.sample.list-scroller-split"})
-    ela = etree.SubElement(el, 'header')
-    ela = etree.SubElement(ela, 'simpleHeader')
-    ela = etree.SubElement(ela, 'title')
-    if PMSroot.get('title1') is not None:
-        ela.text = PMSroot.get('title1')
-    else:
-        ela.text = "Plex Connect"
-    
-    elb = etree.SubElement(el, 'menu') 
-    elb = etree.SubElement(elb, 'sections')
-    elb = etree.SubElement(elb, 'menuSection')
-    
-    elc = etree.SubElement(elb, 'header')
-    elc = etree.SubElement(elc, 'textDivider', {'alignment':'left'})
-    elc = etree.SubElement(elc, 'title')
-    elc.text = "Selection:"
-    
-    eld = etree.SubElement(elb, 'items')
-    # PMS Directories to headline
-    PMS_dirs = PMSroot.findall('Directory')
-    for i in PMS_dirs:
-        key = i.get('key')
-        if key.startswith("/"):
-            el_path = 'http://trailers.apple.com'+key+'/'
-        else:
-            el_path = 'http://trailers.apple.com'+path+key+'/'
-        
-        el__ = etree.SubElement(eld, 'oneLineMenuItem', {'id':i.get('key')})
-        el__.set('onSelect', "atv.loadURL('"+el_path+"')")
-        el__.set('onPlay', "atv.loadURL('"+el_path+"')")
-        
-        el_Z = etree.SubElement(el__, 'label')
-        el_Z.text = i.get('title')
-        el_Z = etree.SubElement(el__, 'preview')
-        el_Z = etree.SubElement(el_Z, 'link')
-        el_Z.text = 'PREVIEW_URL'  # todo:
-
-
-
-def XML_Movie(elem, PMS_XML, path):
-    PMSroot = PMS_XML.getroot()
-    
-    el = etree.SubElement(elem, "body")
-    el_scroller = etree.SubElement(el, "scroller", {'id':"com.sample.movie-grid"})
-    ela = etree.SubElement(el_scroller, 'header')
-    ela = etree.SubElement(ela, 'simpleHeader')
-    ela = etree.SubElement(ela, 'title')
-    ela.text = PMSroot.get('title1')
-    
-    el_items = etree.SubElement(el_scroller, "items")
-    
-    el_grid = etree.SubElement(el_items, "grid", {'id':"grid_0", 'columnCount':"7"})
-    el_items2 = etree.SubElement(el_grid, "items")
-    
-    aTV_shelf_item = 1
-    for i in PMSroot.findall('Video'):
-        key = i.get('key')
-        if key.startswith("/"):
-            el_path = 'http://trailers.apple.com'+key+'/'
-        else:
-            el_path = 'http://trailers.apple.com'+path+key+'/'
-        
-        el_moviePoster=etree.SubElement(el_items2, "moviePoster")
-        el_moviePoster.set('id', 'shelf_item_'+str(aTV_shelf_item))
-        aTV_shelf_item += 1
-        
-        #el_moviePoster.set('accessibilityLabel', i.get('title'))
-        #el_moviePoster.set('related', 'true')
-        
-        el_moviePoster.set('onSelect', "atv.loadURL('"+el_path+"&PlexConnect=Play')")  # todo: 'Select' - show metadata
-        el_moviePoster.set('onPlay', "atv.loadURL('"+el_path+"&PlexConnect=Play')")
-        el = etree.SubElement(el_moviePoster, 'title')
-        el.text = i.get('title')
-        el = etree.SubElement(el_moviePoster, 'subtitle')
-        el.text = i.get('year')
-        el = etree.SubElement(el_moviePoster, 'image')
-        el.text = 'http://' + Addr_PMS + i.get('thumb')  # direct connect to Plex Media Server
-        el = etree.SubElement(el_moviePoster, 'defaultImage')
-        el.text = "resource://16X9.png"
-
-
-
-def XML_Episode(elem, PMS_XML, path):
-    PMSroot = PMS_XML.getroot()
-    
-    el = etree.SubElement(elem, "body")
-    el_listWithPreview = etree.SubElement(el, "listWithPreview", {'id':"com.sample.menu-items-with-sections"})
-    ela = etree.SubElement(el_listWithPreview, 'header')
-    ela = etree.SubElement(ela, 'simpleHeader')
-    elb = etree.SubElement(ela, 'title')
-    elb.text = PMSroot.get('title1')
-    elb = etree.SubElement(ela, 'subtitle')
-    elb.text = PMSroot.get('title2')
-    el_menu = etree.SubElement(el_listWithPreview, "menu")
-    el_sections = etree.SubElement(el_menu, "sections")
-    el_menuSection = etree.SubElement(el_sections, "menuSection")
-    el_items = etree.SubElement(el_menuSection, "items")
-    
-    aTV_shelf_item = 1
-    for i in PMSroot.findall('Video'):
-        key = i.get('key')
-        if key.startswith("/"):
-            el_path = 'http://trailers.apple.com'+key+'/'
-        else:
-            el_path = 'http://trailers.apple.com'+path+key+'/'
-        
-        el_moviePoster=etree.SubElement(el_items, "twoLineEnhancedMenuItem")
-        el_moviePoster.set('id', 'shelf_item_'+str(aTV_shelf_item))
-        aTV_shelf_item += 1
-        
-        #el_moviePoster.set('accessibilityLabel', i.get('title'))
-        #el_moviePoster.set('related', 'true')
-        
-        el_moviePoster.set('onSelect', "atv.loadURL('"+el_path+"&PlexConnect=Play')")  # todo: 'Select' - show metadata
-        el_moviePoster.set('onPlay', "atv.loadURL('"+el_path+"&PlexConnect=Play')")
-        
-        el = etree.SubElement(el_moviePoster, 'label')
-        el.text = i.get('title')
-        el = etree.SubElement(el_moviePoster, 'ordinal')
-        el.text = i.get('index')
-        el = etree.SubElement(el_moviePoster, 'maxOrdinalDigits')
-        el.text = '2'
-        el = etree.SubElement(el_moviePoster, 'image')
-        el.text = 'http://' + Addr_PMS + i.get('thumb')  # direct connect to Plex Media Server
-        el = etree.SubElement(el_moviePoster, 'defaultImage')
-        el.text = "resource://16X9.png"
-        preview = etree.SubElement(el_moviePoster, 'preview')
-        keyedPreview = etree.SubElement(preview, 'keyedPreview')
-        kp = etree.SubElement(keyedPreview, 'title')
-        kp.text = i.get('title')
-        kp = etree.SubElement(keyedPreview, 'summary')
-        kp.text = i.get('summary')
-        kp = etree.SubElement(keyedPreview, 'image')
-        kp.text = 'http://' + Addr_PMS + i.get('thumb')
-
-def XML_TVSeason(elem, PMS_XML, path):
-    PMSroot = PMS_XML.getroot()
-    
-    el = etree.SubElement(elem, "body")
-    el_listWithPreview = etree.SubElement(el, "listWithPreview", {'id':"com.sample.menu-items-with-sections"})
-    ela = etree.SubElement(el_listWithPreview, 'header')
-    ela = etree.SubElement(ela, 'simpleHeader')
-    elb = etree.SubElement(ela, 'title')
-    elb.text = PMSroot.get('title2')
-
-    el_menu = etree.SubElement(el_listWithPreview, "menu")
-    el_sections = etree.SubElement(el_menu, "sections")
-    el_menuSection = etree.SubElement(el_sections, "menuSection")
-    el_items = etree.SubElement(el_menuSection, "items")
-    
-    aTV_shelf_item = 1
-    for i in PMSroot.findall('Directory'):
-        key = i.get('key')
-        if key.startswith("/"):
-            el_path = 'http://trailers.apple.com'+key+'/'
-        else:
-            el_path = 'http://trailers.apple.com'+path+key+'/'
-        
-        el_moviePoster=etree.SubElement(el_items, "oneLineMenuItem")
-        el_moviePoster.set('id', 'shelf_item_'+str(aTV_shelf_item))
-        aTV_shelf_item += 1
-        
-        #el_moviePoster.set('accessibilityLabel', i.get('title'))
-        #el_moviePoster.set('related', 'true')
-        
-        el_moviePoster.set('onSelect', "atv.loadURL('"+el_path+"')")  # todo: 'Select' - show metadata
-        el_moviePoster.set('onPlay', "atv.loadURL('"+el_path+"')")
-        
-        el = etree.SubElement(el_moviePoster, 'label')
-        el.text = i.get('title')
-        pv = etree.SubElement(el_moviePoster, "preview")
-        cf = etree.SubElement(pv, "crossFadePreview")
-        img = etree.SubElement(cf, 'image')
-        img.text = 'http://' + Addr_PMS + i.get('thumb')  # direct connect to Plex Media Server
-       
-def XML_TVShow_ListView(elem, PMS_XML, path):
-    PMSroot = PMS_XML.getroot()
-    
-    el = etree.SubElement(elem, "body")
-    el_listWithPreview = etree.SubElement(el, "listWithPreview", {'id':"com.sample.menu-items-with-sections"})
-    ela = etree.SubElement(el_listWithPreview, 'header')
-    ela = etree.SubElement(ela, 'simpleHeader')
-    elb = etree.SubElement(ela, 'title')
-    elb.text = PMSroot.get('title2')
-
-    el_menu = etree.SubElement(el_listWithPreview, "menu")
-    el_sections = etree.SubElement(el_menu, "sections")
-    el_menuSection = etree.SubElement(el_sections, "menuSection")
-    el_items = etree.SubElement(el_menuSection, "items")
-    
-    aTV_shelf_item = 1
-    for i in PMSroot.findall('Directory'):
-        key = i.get('key')
-        if key.startswith("/"):
-            el_path = 'http://trailers.apple.com'+key+'/'
-        else:
-            el_path = 'http://trailers.apple.com'+path+key+'/'
-        
-        el_moviePoster=etree.SubElement(el_items, "oneLineMenuItem")
-        el_moviePoster.set('id', 'shelf_item_'+str(aTV_shelf_item))
-        aTV_shelf_item += 1
-        
-        #el_moviePoster.set('accessibilityLabel', i.get('title'))
-        #el_moviePoster.set('related', 'true')
-        
-        el_moviePoster.set('onSelect', "atv.loadURL('"+el_path+"')")  # todo: 'Select' - show metadata
-        el_moviePoster.set('onPlay', "atv.loadURL('"+el_path+"')")
-        
-        el = etree.SubElement(el_moviePoster, 'label')
-        el.text = i.get('title')
-        pv = etree.SubElement(el_moviePoster, "preview")
-        cf = etree.SubElement(pv, "crossFadePreview")
-        img = etree.SubElement(cf, 'image')
-        img.text = 'http://' + Addr_PMS + i.get('thumb')  # direct connect to Plex Media Server
-        
 """
 <atv>
   <body>
@@ -403,40 +184,155 @@ def XML_PMS2aTV(address, path):
     PMS = XML_ReadFromURL(address, path)
     PMSroot = PMS.getroot()
     
-    el_aTV = etree.Element("atv")
+    #el_aTV = etree.Element("atv")
     if PMSroot.get('viewGroup') is None or \
        PMSroot.get('viewGroup')=='secondary':
-        XML_Directory(el_aTV, PMS, path)
-       
+        aTVTree = etree.parse(curdir+'/assets/templates/Directory.xml')
+        aTVroot = aTVTree.getroot()
+        XML_Expand(aTVroot, PMSroot, path)
+        
     elif PMSroot.get('viewGroup')=='show':
         # TV Show grid view
-        XML_TVShow_ListView(el_aTV, PMS, path)
+        aTVTree = etree.parse(curdir+'/assets/templates/Show.xml')
+        aTVroot = aTVTree.getroot()
+        XML_Expand(aTVroot, PMSroot, path)
         
     elif PMSroot.get('viewGroup')=='season':
         # TV season view
-        XML_TVSeason(el_aTV, PMS, path)
+        aTVTree = etree.parse(curdir+'/assets/templates/Season.xml')
+        aTVroot = aTVTree.getroot()
+        XML_Expand(aTVroot, PMSroot, path)
         
     elif PMSroot.get('viewGroup')=='movie':
         # movie listing
-        XML_Movie(el_aTV, PMS, path)
-    
+        aTVTree = etree.parse(curdir+'/assets/templates/Movie.xml')
+        aTVroot = aTVTree.getroot()
+        XML_Expand(aTVroot, PMSroot, path)
+        
     elif PMSroot.get('viewGroup')=='episode':
         # TV episode view
-        XML_Episode(el_aTV, PMS, path)
+        aTVTree = etree.parse(curdir+'/assets/templates/Episode.xml')
+        aTVroot = aTVTree.getroot()
+        XML_Expand(aTVroot, PMSroot, path)
     
     # todo: channels, photos...
     
-    aTVTree = etree.ElementTree(el_aTV)    
+    #aTVTree = etree.ElementTree(el_aTV)    
     
     dprint(__name__, 1, "====== generated aTV-XML ======")
     dprint(__name__, 1, XML_prettystring(aTVTree))
     dprint(__name__, 1, "====== aTV-XML finished ======")
     
-    return etree.tostring(el_aTV)
+    return etree.tostring(aTVroot)
+
+
+
+def Path_addPath(path, addpath):
+    if addpath.startswith("/"):
+        res = addpath+'/'
+    else:
+        res = path+addpath+'/'
+    return res
+
+
+
+def XML_ExpandLine(elem, src, path, line):
+    while True:
+        cmd_start = line.find('{{')
+        cmd_end   = line.find('}}')
+        if cmd_start==-1 or cmd_end==-1:
+            break;
+        
+        cmd = line[cmd_start+2:cmd_end]
+        if cmd.startswith('VAL('):
+            cmd = cmd[len('VAL('):]
+            param = string.strip(cmd, '[]()').split(':')
+            #print "VAL", param[0], param[1], line
+            if param[0]=='tag':  # todo
+                res = ""
+            elif param[0]=='attrib':
+                res = src.get(param[1],'')
+        
+        elif cmd.startswith('ADDPATH('):
+            cmd = cmd[len('ADDPATH('):]
+            param = string.strip(cmd, '[]()').split(':')
+            #print "ADDPATH", param[0], param[1], line
+            if param[0]=='tag':  # todo
+                res = ""
+            elif param[0]=='attrib':
+                res = Path_addPath(path, src.get(param[1],''))
+        
+        elif cmd=='ADDR_PMS':
+            res = Addr_PMS
+    
+        else:
+            res = ""  # unsupported
+        
+        line = line[:cmd_start] + res + line[cmd_end+2:]
+    
+    return line
+
+
+
+def XML_Expand(elem, src, path):
+
+    # unpack template 'COPY' command in children
+    for child in elem:
+        line = child.text
+        if line==None: line=''
+        cmd_start = line.find('{{')
+        cmd_end   = line.find('}}')    
+        if cmd_start>-1 and cmd_end>-1:
+            cmd = line[cmd_start+2:cmd_end]
+            if cmd.startswith('COPY('):
+                cmd = cmd[len('COPY('):]
+                param = string.strip(cmd, '[]()').split(':')
+
+                childToCopy = child
+                elem.remove(child)
+                childToCopy.text = line[:cmd_start] + line[cmd_end+2:]  # remove current cmd
+                # duplicate child and add to tree                
+                if param[0]=='tag':
+                    for elemSRC in src.findall(param[1]):  # tag
+                        el = copy.deepcopy(childToCopy)
+                        XML_Expand(el, elemSRC, path)
+                        elem.append(el)
+                elif param[0]=='attrib':
+                    pass  # todo
+    
+    # unpack template commands in elem.text
+    line = elem.text
+    if line==None: line=''
+    elem.text = XML_ExpandLine(elem, src, path, line)
+    
+    # unpack template commands in elem.attrib.value
+    for attrib in elem.attrib:
+        line = elem.get(attrib)
+        elem.set(attrib, XML_ExpandLine(elem, src, path, line))
+ 
+    # recurse into children
+    for el in elem:
+        XML_Expand(el, src, path)
 
 
 
 if __name__=="__main__":
-    # todo: Testcode: load PMS-XML from file, convert to aTV-XML
-    # change the way XML_ReadFromURL() is used...
-    pass
+    print "load PMS XML"    
+    PMSTree = etree.parse(curdir+'/XML/PMS_Season.xml')
+    PMSroot = PMSTree.getroot()
+    #XML_prettyprint(PMStree)
+
+    print "load aTV XML template"
+    aTVTree = etree.parse(curdir+'/assets/templates/Season.xml')
+    aTVroot = aTVTree.getroot()
+    #XML_prettyprint(aTVtree)
+
+    print "unpack PlexConnect commands"
+    XML_Expand(aTVroot, PMSroot, '/library/sections/')
+    #XML_prettyprint(aTVtree)
+
+    print "store aTV XML"
+    str = XML_prettystring(aTVTree)
+    f=open(curdir+'/XML/aTV_fromTmpl.xml', 'w')
+    f.write(str)
+    f.close()
