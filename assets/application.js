7be4b38166448a39532b08f9cee364f48c085122
roidy <roidy.parnell@ntlworld.com>
2013-04-23 19:35:19 +0100

    Watched flags now work
    Need to edit application.js with your own PMS IP.

--- assets/application.js
+++ assets/application.js
@@ -1,3 +1,29 @@
+var watchedPoint;
+var PMS = "http://192.168.0.200:32400";
+
+function loadPage(url)
+{
+	var req = new XMLHttpRequest();
+	req.open('GET', url, true);
+	req.send();
+}
+
+atv.player.playerTimeDidChange = function(time)
+{
+        if (time>watchedPoint)
+        {
+            watchedPoint = watchedPoint * 2; // hack to stop it from repeatedly setting the Watched Flag.
+            //atv.loadURL(PMS + "/:/scrobble?key=" + atv.sessionStorage['ratingKey'] + "&identifier=com.plexapp.plugins.library"); //Crap can't use atv.page loader
+            loadPage(PMS + "/:/scrobble?key=" + atv.sessionStorage['ratingKey'] + "&identifier=com.plexapp.plugins.library"); // So we'll roll our own :)
+        }
+};
+
+atv.player.willStartPlaying = function()
+{	
+        watchedPoint = parseInt(atv.sessionStorage['duration']); // Grab video duration from python server.
+        watchedPoint = ((watchedPoint/100000)*95); // Calculate the 95% time point in seconds.
+};
+
 atv.config = { 
     "doesJavaScriptLoadRoot": true,
     "DEBUG_LEVEL": 4,
