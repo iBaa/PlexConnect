560c3d291fea8bfd386bea203cc185e7ac6f6c77
roidy <roidy.parnell@ntlworld.com>
2013-04-26 18:48:14 +0100

    Added the start of resume time code
    iBaa, please see the thread I posted on glassboard about getting the
    resume time key from the plex library.

--- assets/application.js
+++ assets/application.js
@@ -1,4 +1,6 @@
 var watchedPoint;
+var watchedSet = false;
+var resumePoint;
 var PMS = "http://192.168.0.200:32400";
 
 function loadPage(url)
@@ -10,18 +12,29 @@ function loadPage(url)
 
 atv.player.playerTimeDidChange = function(time)
 {
+    if (!watchedSet)
+    {
         if (time>watchedPoint)
         {
             watchedPoint = watchedPoint * 2; // hack to stop it from repeatedly setting the Watched Flag.
             //atv.loadURL(PMS + "/:/scrobble?key=" + atv.sessionStorage['ratingKey'] + "&identifier=com.plexapp.plugins.library"); //Crap can't use atv.page loader
             loadPage(PMS + "/:/scrobble?key=" + atv.sessionStorage['ratingKey'] + "&identifier=com.plexapp.plugins.library"); // So we'll roll our own :)
+            loadPage(PMS + "/:/progress?key=" + atv.sessionStorage['ratingKey'] + "&identifier=com.plexapp.plugins.library&time=0"); // We've watched the file so set resume point to 0 seconds
+            watchedSet = true;
         }
+        else
+        {
+            resumePoint = Math.round(time*1000);
+            loadPage(PMS + "/:/progress?key=" + atv.sessionStorage['ratingKey'] + "&identifier=com.plexapp.plugins.library&time=" + resumePoint.toString());
+        }
+    }
 };
 
 atv.player.willStartPlaying = function()
 {	
         watchedPoint = parseInt(atv.sessionStorage['duration']); // Grab video duration from python server.
         watchedPoint = ((watchedPoint/100000)*95); // Calculate the 95% time point in seconds.
+        watchedSet = false;
 };
 
 atv.config = { 
