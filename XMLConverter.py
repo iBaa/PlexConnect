101b3b5a3ec8c5d92d8eed57aa6bcc36d307641e
iBaa <bernhard.baaser@gmx.de>
2013-05-31 20:14:11 +0200

    fix typo - re-enable transcoding :-D

--- XMLConverter.py
+++ XMLConverter.py
@@ -741,7 +741,7 @@ class CCommandCollection(CCommandHelper):
         # check "Media" element and get key
         if el!=None:  # Media
             if g_ATVSettings.getSetting(UDID, 'forcedirectplay')=='True' or \
-               g_ATVSettings.getSetting(UDID, 'forcetranscoding')!='True' and \
+               g_ATVSettings.getSetting(UDID, 'forcetranscode')!='True' and \
                el.get('container','') in ("mov", "mp4", "mpegts") and \
                el.get('videoCodec','') in ("mpeg4", "h264", "drmi") and \
                el.get('audioCodec','') in ("aac", "ac3", "drms"):
