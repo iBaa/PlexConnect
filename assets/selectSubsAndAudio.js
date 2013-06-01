1937dd4d559bca2ef63238532fc4555cdabe99f2
roidy <roidy.parnell@ntlworld.com>
2013-06-01 09:40:15 +0100

    Added audio stream select + code tidy up.

--- assets/selectSubsAndAudio.js
+++ assets/selectSubsAndAudio.js
@@ -19,38 +19,57 @@ if(atv.Element)
 };
 
 function selectSubs(addrPMS, ratingKey) {
-  var xmlstr = '<?xml version="1.0" encoding="UTF-8"?><atv><head><script src="http://trailers.apple.com/selectSubsAndAudio.js"/></head>\
-                <body><optionList id="selectSubs.optionDialog"><title>Select Subtitle Track</title><items>\
-                <oneLineMenuItem id="none" onSelect="selectSubStream($$addrPMS$$, $$ratingKey$$, 0);atv.unloadPage();"><label>None</label></oneLineMenuItem>\
-                $$subStreams$$\
-                </items></optionList></body></atv>';
-  xmlstr = xmlstr.replace("$$addrPMS$$", "'" + addrPMS + "'");
-  xmlstr = xmlstr.replace("$$ratingKey$$", "'" + ratingKey + "'");
-  var insXml = ''
+  var xmlstr = '<?xml version="1.0" encoding="UTF-8"?><atv><head><script src="http://trailers.apple.com/selectSubsAndAudio.js"/></head> \
+                <body><optionList id="selectSubs.optionDialog"><title>Select Subtitle Track</title><items> \
+                <oneLineMenuItem id="none" onSelect="selectSubStream(\'' + addrPMS + '\', \'' + ratingKey + '\', 0);atv.unloadPage();"><label>None</label></oneLineMenuItem>';
+
+  var streams = document.evaluateXPath('//stream');
   
-	var streams = document.evaluateXPath('//stream');
-  
-	for(var i = 0; i < streams.length; ++i)
+  for(var i = 0; i < streams.length; ++i)
 	{
-		var stream = streams[i]
-    
+    var stream = streams[i]
     if (stream.getElementByTagName('streamType').textContent == '3')
     { 
-      insXml = insXml + '<oneLineMenuItem id="' + stream.getElementByTagName('id').textContent + '" onSelect="selectSubStream($$addrPMS$$, $$ratingKey$$, ' + stream.getElementByTagName('id').textContent + ');atv.unloadPage();">';
-      insXml = insXml + '<label>' + stream.getElementByTagName('language').textContent + ' (' + stream.getElementByTagName('format').textContent + ')</label>';
-      insXml = insXml + '</oneLineMenuItem>';
-      insXml = insXml.replace("$$addrPMS$$", "'" + addrPMS + "'");
-      insXml = insXml.replace("$$ratingKey$$", "'" + ratingKey + "'");
-    }  
+      xmlstr = xmlstr + '<oneLineMenuItem id="substream" onSelect="selectSubStream(\'' + addrPMS + '\', \'' + ratingKey + '\', ' + stream.getElementByTagName('id').textContent + ');atv.unloadPage();">';
+      xmlstr = xmlstr + '<label>' + stream.getElementByTagName('language').textContent + ' (' + stream.getElementByTagName('format').textContent + ')</label>';
+      xmlstr = xmlstr + '</oneLineMenuItem>';
+    }
   }
+  xmlstr = xmlstr + '</items></optionList></body></atv>';
+  var doc = atv.parseXML(xmlstr);
+  atv.loadXML(doc);
+};
+
+function selectAudio(addrPMS, ratingKey) {
+  var xmlstr = '<?xml version="1.0" encoding="UTF-8"?><atv><head><script src="http://trailers.apple.com/selectSubsAndAudio.js"/></head> \
+                <body><optionList id="selectSubs.optionDialog"><title>Select Audio Track</title><items>';
+
+  var streams = document.evaluateXPath('//stream');
   
-  xmlstr = xmlstr.replace("$$subStreams$$", insXml);
+  for(var i = 0; i < streams.length; ++i)
+	{
+    var stream = streams[i]
+    if (stream.getElementByTagName('streamType').textContent == '2')
+    { 
+      xmlstr = xmlstr + '<oneLineMenuItem id="substream" onSelect="selectAudioStream(\'' + addrPMS + '\', \'' + ratingKey + '\', ' + stream.getElementByTagName('id').textContent + ');atv.unloadPage();">';
+      xmlstr = xmlstr + '<label>' + stream.getElementByTagName('language').textContent + ' (' + stream.getElementByTagName('codec').textContent + ')</label>';
+      xmlstr = xmlstr + '</oneLineMenuItem>';
+    }
+  }
+  xmlstr = xmlstr + '</items></optionList></body></atv>';
   var doc = atv.parseXML(xmlstr);
   atv.loadXML(doc);
 };
 
 function selectSubStream(addrPMS, ratingKey, stream) {
   var url = "http://" + addrPMS + "/library/parts/" + ratingKey + "?subtitleStreamID=" + stream.toString()
+  var req = new XMLHttpRequest();
+	req.open('PUT', url, true);
+	req.send();
+};
+
+function selectAudioStream(addrPMS, ratingKey, stream) {
+  var url = "http://" + addrPMS + "/library/parts/" + ratingKey + "?audioStreamID=" + stream.toString()
   var req = new XMLHttpRequest();
 	req.open('PUT', url, true);
 	req.send();
