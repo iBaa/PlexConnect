748598262b3cd03f56faaac4e1c83b7e75765d00
iBaa <bernhard.baaser@gmx.de>
2013-06-15 20:52:59 +0200

    add expand_Date in python

---
+++ expand_Date.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+
+"""
+#! /usr/bin/env ruby
+data = STDIN.read
+last_date = `git log --pretty=format:"%ad" -1`
+puts data.gsub('$Date$', '$Date: ' + last_date.to_s + '$')
+"""
+
+import sys
+from subprocess import call
+
+data = sys.stdin.read()
+last_date = call('git log --pretty=format:"%ad" -1')
+
+sys.stdout.write(data.replace('$Date$', '$Date: ' + last_date.to_s + '$'))
