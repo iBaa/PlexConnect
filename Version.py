70bd8e5cf81c3244185aa79a9ea0aa24b3b2537e
iBaa <bernhard.baaser@gmx.de>
2013-06-15 18:27:49 +0200

    more IDs

--- Version.py
+++ Version.py
@@ -1,3 +1,25 @@
 #!/usr/bin/python
 
+"""
+Option  Description of Output
+%H  Commit hash
+%h  Abbreviated commit hash
+%T  Tree hash
+%t  Abbreviated tree hash
+%P  Parent hashes
+%p  Abbreviated parent hashes
+%an Author name
+%ae Author e-mail
+%ad Author date (format respects the --date= option)
+%ar Author date, relative
+%cn Committer name
+%ce Committer email
+%cd Committer date
+%cr Committer date, relative
+%s  Subject
+"""
+
 last_commit = '$Format:%cd$'
+commit_hash = '$Format:%H$'
+tree_hash = '$Format:%T$'
+parent_hashes = '$Format:%P$'
