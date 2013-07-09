9161ad72019cbd5aed4873fdf74ddff723ba4d17
Bert Wynants <bwynants+drupal-git@gmail.com>
2013-07-09 22:22:54 +0200

    added possibility for PlexConnect to start as linux daemon
    normal usage:
     PlexConnect -logfile /var/PlexConnect.log
    
    to stop it:
     type CTRL-C in console
    
    daemon usage:
     PlexConnect_daemon.py -logfile /var/PlexConnect.log -pidfile /var/PlexConnect.pid
    
    to stop it:
     kill `cat /var/PlexConnect.pid`

--- PlexConnect.py
+++ PlexConnect.py
@@ -8,104 +8,189 @@ inter-process-communication (queue): http://pymotw.com/2/multiprocessing/communi
 """
 
 
-import sys, time
-from os import sep
+import sys
+import time
+import os
 import socket
 from multiprocessing import Process, Pipe
-import signal, errno
+import signal
+import errno
+import getopt
 
-import DNSServer, WebServer
+import DNSServer
+import WebServer
 import Settings
 from Debug import *  # dprint()
 
 
+class PlexConnect(object):
 
-def getIP_self():
-    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    s.connect(('1.2.3.4', 1000))
-    IP = s.getsockname()[0]
-    dprint('PlexConnect', 0, "IP_self: "+IP)
-    return IP
+    def __init__(self, *args, **kwargs):
+        self.running = False
+        self.logfile = sys.path[0] + os.sep + 'PlexConnect.log'
+        self.pipes = []
+        self.processes = []
+        self.param = {}
+        self.parse_opts(sys.argv[1:])
 
+    def getIP(self):
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        s.connect(('1.2.3.4', 1000))
+        IP = s.getsockname()[0]
+        dprint('PlexConnect', 0, "IP_self: " + IP)
+        return IP
 
+    def get_opts_short(self):
+        """
+        getopts short options
+        override, call bass class and add your own short opts [string]
+        """
+        return "l:"
 
-g_shutdown = False
+    def get_opts_long(self):
+        """
+        getopts long options
+        override, call bass class and add your own long opts [list]
+        """
+        return ['logfile=']
 
-def sighandler_shutdown(signum, frame):
-    dprint('PlexConnect', 0,  "Shutting down.")
-    global g_shutdown
-    g_shutdown = True
-    if pipe_DNSServer != None:
-        pipe_DNSServer[0].send('shutdown')
-    if pipe_WebServer != None:
-        pipe_WebServer[0].send('shutdown')
+    def get_opts_clarification(self):
+        """
+        getopts clarification
+        override, call bass class and add your own clarification for opts [string]
+        """
+        return "[--logfile filename]"
 
+    def parse_opts(self, args):
+        """
+        Parse the argumets passed onto the application
+        """
+        try:
+            opts, args = getopt.getopt(args, self.get_opts_short(), self.get_opts_long())  # @UnusedVariable
+            self.handle_opts(opts)
+        except getopt.GetoptError:
+            print("Available Options:  %s" % self.get_opts_clarification())
+            sys.exit(0)
 
+    def handle_opts(self, opts):
+        """
+        handle arguments
+        override, call bass class and add your own argument handling
+        """
+        for o, a in opts:
+            if o in ('-l', '--logfile'):
+                self.logfile = str(a)
 
-if __name__=="__main__":
-    signal.signal(signal.SIGINT, sighandler_shutdown)
-    signal.signal(signal.SIGTERM, sighandler_shutdown)
-    
-    pipe_DNSServer = None
-    pipe_WebServer = None
-    
-    param = {}
-    param['LogFile'] = sys.path[0] + sep + 'PlexConnect.log'
-    dinit('PlexConnect', param, True)  # init logging, new file, main process
+    def stop(self):
+        """
+        Stop PlexConnect
+        """
+        self.running = False
+        # send shutdown to all pipes
+        for pipe in self.pipes:
+            pipe.send('shutdown')
 
-    dprint('PlexConnect', 0, "***")
-    dprint('PlexConnect', 0, "PlexConnect")
-    dprint('PlexConnect', 0, "Press CTRL-C to shut down.")
-    dprint('PlexConnect', 0, "***")
-    
-    # Settings
-    cfg = Settings.CSettings()
-    param['CSettings'] = cfg
-    
-    param['IP_self'] = getIP_self()
-    param['HostToIntercept'] = 'trailers.apple.com'
-    
-    # Logfile, re-init
-    param['LogLevel'] = cfg.getSetting('loglevel')
-    dinit('PlexConnect', param)  # re-init logfile with loglevel
-    
-    # init DNSServer
-    if cfg.getSetting('enable_dnsserver')=='True':
-        pipe_DNSServer = Pipe()  # endpoint [0]-PlexConnect, [1]-DNSServer
-        p_DNSServer = Process(target=DNSServer.Run, args=(pipe_DNSServer[1], param))
-        p_DNSServer.start()
-    
-        time.sleep(0.1)
-        if not p_DNSServer.is_alive():
-            dprint('PlexConnect', 0, "DNSServer not alive. Shutting down.")
+    def cleanup(self):
+        """
+        Cleanup PlexConnect
+        """
+        # join all processes
+        for process in self.processes:
+            process.join()
+        dprint('PlexConnect', 0, "shutdown")
+
+    # initialize the settings and startup all needed processes
+    def start(self):
+        """
+        Start PlexConnect
+        """
+        # setup logfile location
+        self.param['LogFile'] = self.logfile
+
+        dinit('PlexConnect', self.param, True)  # init logging, new file, main process
+
+        # Settings
+        cfg = Settings.CSettings()
+        self.param['CSettings'] = cfg
+
+        self.param['IP_self'] = self.getIP()
+        self.param['HostToIntercept'] = 'trailers.apple.com'
+
+        # Logfile, re-init
+        self.param['LogLevel'] = cfg.getSetting('loglevel')
+        dinit('PlexConnect', self.param)  # re-init logfile with loglevel
+
+        self.running = True
+
+        if cfg.getSetting('enable_dnsserver') == 'True':
+            sender, receiver = Pipe()  # endpoint [0]-PlexConnect, [1]-DNSServer
+            process = Process(target=DNSServer.Run, args=(receiver, self.param))
+            process.start()
+
+            time.sleep(0.1)
+            if not process.is_alive():
+                dprint('PlexConnect', 0, "DNSServer not alive. Shutting down.")
+                self.running = false
+            else:
+                self.pipes.append(sender)
+                self.processes.append(process)
+
+        if (self.running):
+            # init WebServer
+            sender, receiver = Pipe()  # endpoint [0]-PlexConnect, [1]-WebServer
+            process = Process(target=WebServer.Run, args=(receiver, self.param))
+            process.start()
+
+            time.sleep(0.1)
+            if not process.is_alive():
+                dprint('PlexConnect', 0, "WebServer not alive. Shutting down.")
+                self.running = false
+            else:
+                self.pipes.append(sender)
+                self.processes.append(process)
+
+        # not started successful
+        if (not self.running):
+            # stop the parts that did start
+            self.stop()
+            self.cleanup()
             sys.exit(1)
-    
-    # init WebServer
-    pipe_WebServer = Pipe()  # endpoint [0]-PlexConnect, [1]-WebServer
-    p_WebServer = Process(target=WebServer.Run, args=(pipe_WebServer[1], param))
-    p_WebServer.start()
-    
-    time.sleep(0.1)
-    if not p_WebServer.is_alive():
-        dprint('PlexConnect', 0, "WebServer not alive. Shutting down.")
-        if cfg.getSetting('enable_dnsserver')=='True':
-            pipe_DNSServer[0].send('shutdown')
-            p_DNSServer.join()
-        sys.exit(1)
-    
-    # work until shutdown
-    # ...or just wait until child processes are done
-    while g_shutdown==False:
+
+    def loop(self):
         # do something important
         try:
             time.sleep(60)
         except IOError as e:
-            if e.errno == errno.EINTR and g_shutdown == True:
+            if e.errno == errno.EINTR and not self.running:
                 pass  # mask "IOError: [Errno 4] Interrupted function call"
             else:
                 raise
-    
-    if cfg.getSetting('enable_dnsserver')=='True':
-        p_DNSServer.join()
-    
-    p_WebServer.join()
+
+    def run(self):
+        while self.running:
+            self.loop()
+
+
+def sighandler_shutdown(signum, frame):
+    signal.signal(signal.SIGINT, signal.SIG_IGN)  # we heard you!
+    plexConnect.stop()
+
+if __name__ == "__main__":
+    signal.signal(signal.SIGINT, sighandler_shutdown)
+    signal.signal(signal.SIGTERM, sighandler_shutdown)
+
+    plexConnect = PlexConnect()
+
+    # write out some help text
+    dprint('PlexConnect', 0, "***")
+    dprint('PlexConnect', 0, "PlexConnect")
+    dprint('PlexConnect', 0, "Press CTRL-C to shut down.")
+    dprint('PlexConnect', 0, "***")
+
+    plexConnect.start()
+
+    plexConnect.run()
+
+    plexConnect.cleanup()
+
+    sys.exit(0)
