1c26a11f127bf3d214df8fc378be6cdf13caa76c
Samer Albahra <salbahra@gmail.com>
2013-10-26 09:24:38 -0500

    Add trailer button to MoviePrePlay templete
    Looks up trailer using Youtube if trailer button is pushed and loads
    trailer using the YouTube channel.

--- assets/js/utils.js
+++ assets/js/utils.js
@@ -81,6 +81,54 @@ function loadMenuPage(event)
 
 
 /*
+ * translate movie title into trailer URL for playback
+ */
+function playTrailer(addrPMS,title)
+{
+    log("playTrailer: "+title);
+    var lookup = "https://gdata.youtube.com/feeds/api/videos?q="+encodeURIComponent(title)+"+trailer&start-index=1&max-results=1&v=2&alt=json&hd";
+
+    var req = new XMLHttpRequest();
+    req.onreadystatechange = function()
+    {
+        try
+        {
+            if(req.readyState == 4)
+            {
+                var doc = JSON.parse(req.responseText);
+                var links = doc.feed.entry[0].link;
+
+                for (var i = 0; i < links.length; i++) {
+                    if (links[i].type == "text/html") {
+                        var pattern = /watch\?v=(\w+)&/i;
+                        var video = pattern.exec(links[i].href)[1];
+                        break;
+                    }
+                };
+
+                var url = "http://atv.plexconnect/PMS("+encodeURIComponent(addrPMS)+")/system/:/services/url/lookup?url=http%3A//www.youtube.com/watch%3Fv%3D"+video+"&PlexConnect=Play";
+
+                if (url.indexOf("atv.plexconnect")!=-1)
+                {
+                    url = url + "&PlexConnectUDID=" + atv.device.udid;
+                    url = url + "&PlexConnectATVName=" + encodeURIComponent(atv.device.displayName);
+                }
+                
+                iOS_atv_loadURL(url);
+            }
+        }
+        catch(e)
+        {
+            req.abort();
+        }
+    }
+    req.open('GET', lookup, true);
+    req.send();
+};
+
+
+
+/*
  * override stock atv.loadURL() function, adding UDID if directed to PlexConnect
  */
 var iOS_atv_loadURL = atv.loadURL;
