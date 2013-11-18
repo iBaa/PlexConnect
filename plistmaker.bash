10d36266d7d1b17a739912acef1ebb187beddd00
Dave Marston <dlmarston@gmail.com>
2013-11-18 23:05:57 +1100

    added in SSL certificate creation

--- plistmaker.bash
+++ plistmaker.bash
@@ -11,6 +11,10 @@ sed -e "s/__REPLACE_THIS_PATH__/${ourpath}/" ./com.plex.plexconnect_daemon.bash.
 chown root /Library/LaunchDaemons/com.plex.plexconnect_daemon.bash.plist
 chmod 644 /Library/LaunchDaemons/com.plex.plexconnect_daemon.bash.plist
 
+## create the required SSL certificates
+openssl req -new -nodes -newkey rsa:2048 -out ./assets/certificates/trailers.pem -keyout ./assets/certificates/trailers.key -x509 -days 7300 -subj "/C=US/CN=trailers.apple.com"
+openssl x509 -in ./assets/certificates/trailers.pem -outform der -out ./assets/certificates/trailers.cer && cat ./assets/certificates/trailers.key >> ./assets/certificates/trailers.pem
+
 ## launch the plist so that we can use it without a reboot
 launchctl load /Library/LaunchDaemons/com.plex.plexconnect_daemon.bash.plist
 
