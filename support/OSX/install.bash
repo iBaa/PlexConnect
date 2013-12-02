200c9791e7cfc2f1323f58d064ec9bd290f5933f
iBaa <bernhard.baaser@gmx.de>
2013-12-02 20:57:33 +0100

    OSX starter: merge changes from branch OSXautostart (own Pull Req #195)
    - remove daemon startup (4482d06461)
    - add launchctl/plist to start PlexConnect.py directly (d9bc564aec)

--- support/OSX/install.bash
+++ support/OSX/install.bash
@@ -19,27 +19,28 @@ else
 fi
 
 ## create autostart plist for next boot
-echo 'Installing PlexConnect_daemon...'
+echo 'Installing PlexConnect...'
 
 ## Escape the path for use with sed 
-ourpath="${PWD//\//\\/}"
+PlexConnectPath="${PWD//\//\\/}"
 
 ## replace __REPLACE_THIS_PATH__ in default com.plex.plexconnect.daemon.bash.plist
 ## save directly to the /Library/LaunchDameons folder
-sed -e "s/__REPLACE_THIS_PATH__/${ourpath}/" ./support/OSX/com.plex.plexconnect_daemon.bash.plist > /Library/LaunchDaemons/com.plex.plexconnect_daemon.bash.plist
+sed -e "s/__REPLACE_THIS_PATH__/${PlexConnectPath}/" ./support/OSX/com.plex.plexconnect.bash.plist > /Library/LaunchDaemons/com.plex.plexconnect.bash.plist
 
 ## change ownership and permissions of the plist file to make it launchctl compatible
-chown root /Library/LaunchDaemons/com.plex.plexconnect_daemon.bash.plist
-chmod 644 /Library/LaunchDaemons/com.plex.plexconnect_daemon.bash.plist
+chown root /Library/LaunchDaemons/com.plex.plexconnect.bash.plist
+chmod 644 /Library/LaunchDaemons/com.plex.plexconnect.bash.plist
 
 ## start PlexConnect for this session
 echo 'Starting PlexConnect...'
 
 ## launch the plist so that we can use it without a reboot
-launchctl load /Library/LaunchDaemons/com.plex.plexconnect_daemon.bash.plist
+launchctl load /Library/LaunchDaemons/com.plex.plexconnect.bash.plist
 
 ## wait a second to allow PlexConnect to load
 sleep 1
 
 ## display the running status of PlexConnect
-./PlexConnect_daemon.bash status
+#./PlexConnect_daemon.bash status
+launchctl list | grep com.plex.plexconnect.bash
