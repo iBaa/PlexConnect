171e0067224e6f23ae66b4725dca3a2b9869ce6b
iBaa <bernhard.baaser@gmx.de>
2013-12-08 11:37:18 +0100

    OSX Installer - optimize pwd usage, fix cert echo issue

--- support/OSX/install.bash
+++ support/OSX/install.bash
@@ -1,19 +1,24 @@
 #!/bin/bash
 
-## cd to PlexConnect main directory
-cd "$( cd "$( dirname "$0" )" && pwd )"/../..
+## save path to installer files
+cd "$( cd "$( dirname "$0" )" && pwd )"
+InstallerPath=${PWD}
+
+## find PlexConnect main path
+cd ../..
+PlexConnectPath=${PWD}
 
 ## check SSL certificate
-file="assets/certificates/trailers.cer"
+file="${PlexConnectPath}/assets/certificates/trailers.cer"
 if [ -f "$file" ]; then
     echo 'SSL certificate found' 
 
 else
     ## if not, print a reminder
-    echo 'SSL certificate not found
+    echo 'SSL certificate not found'
     echo '- is it stored in a different place?'
     echo '  -> make sure to edit Settings.cfg and restart PlexConnect'
-    echo '- did you already create one?
+    echo '- did you already create one?'
     echo '  -> run createcert.bash'
 
 fi
@@ -21,12 +26,9 @@ fi
 ## create autostart plist for next boot
 echo 'Installing PlexConnect...'
 
-## Escape the path for use with sed 
-PlexConnectPath="${PWD//\//\\/}"
-
 ## replace __REPLACE_THIS_PATH__ in default com.plex.plexconnect.daemon.bash.plist
 ## save directly to the /Library/LaunchDameons folder
-sed -e "s/__REPLACE_THIS_PATH__/${PlexConnectPath}/" ./support/OSX/com.plex.plexconnect.bash.plist > /Library/LaunchDaemons/com.plex.plexconnect.bash.plist
+sed -e "s/__REPLACE_THIS_PATH__/${PlexConnectPath//\//\\/}/" "${InstallerPath}/com.plex.plexconnect.bash.plist" > /Library/LaunchDaemons/com.plex.plexconnect.bash.plist
 
 ## change ownership and permissions of the plist file to make it launchctl compatible
 chown root /Library/LaunchDaemons/com.plex.plexconnect.bash.plist
