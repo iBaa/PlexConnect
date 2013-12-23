54e58a7c38f2105b1c1bcf819c6f09bb6f5bb19d
wahlmanj <wahlman.j@gmail.com>
2013-12-23 12:01:40 -0600

    Update PlexConnect.bash
    Installing cydelete from cydia allows the default PlexConnect.bash script to function. Changed back to default PlexConnect.bash

--- support/IOS_jailbreak/PlexConnect.bash
+++ support/IOS_jailbreak/PlexConnect.bash
@@ -4,12 +4,39 @@
 # OSX PlexConnect startup script
 #
 
-# Run in a loop until successfully connected to the internet
-until wget -q -O - http://www.google.com | grep Lucky > /dev/null; do
-sleep 10
-done
-exec $1&
+# Package
+APPNAME="PlexConnect.py"
 
-# Change directory & launch plexconnect
-cd /Applications/PlexConnect
-./PlexConnect.py 
+
+# Determine if the network is up by looking for any non-loopback network interfaces.
+# Currently supports only OSX "Darwin" OS
+CheckForNetwork()
+{
+    local test
+    
+    if [ -z "${NETWORKUP:=}" ]; then
+        test=$(ifconfig -a inet 2>/dev/null | sed -n -e '/127.0.0.1/d' -e '/0.0.0.0/d' -e '/inet/p' | wc -l)
+        if [ "${test}" -gt 0 ]; then
+            NETWORKUP="-YES-"
+        else
+            NETWORKUP="-NO-"
+        fi
+    fi
+}
+
+
+# Wait for network readiness.
+# This avoids the binding of PlexConnect to the loopback address which may otherwise occur
+# if the bash script is called from a LaunchDaemon/plist file at boot time.
+
+CheckForNetwork
+while [ "${NETWORKUP}" != "-YES-" ]
+do
+    sleep 5
+    NETWORKUP=
+    CheckForNetwork
+done
+
+
+# Start PlexConnect
+./${APPNAME}
