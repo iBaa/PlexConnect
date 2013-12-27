74caf99c9437279124d3955a9da5a30ba493eaf9
wahlmanj <wahlman.j@gmail.com>
2013-12-27 14:41:54 -0600

    Update PlexConnect.bash

--- support/IOS_jailbreak/PlexConnect.bash
+++ support/IOS_jailbreak/PlexConnect.bash
@@ -1,42 +1,16 @@
 #!/bin/bash
 
 #
-# OSX PlexConnect startup script
+# OSX PlexConnect startup script thanks to @stonegray on #jailbreakqa
 #
 
-# Package
-APPNAME="PlexConnect.py"
-
-
-# Determine if the network is up by looking for any non-loopback network interfaces.
-# Currently supports only OSX "Darwin" OS
-CheckForNetwork()
-{
-    local test
-    
-    if [ -z "${NETWORKUP:=}" ]; then
-        test=$(ifconfig -a inet 2>/dev/null | sed -n -e '/127.0.0.1/d' -e '/0.0.0.0/d' -e '/inet/p' | wc -l)
-        if [ "${test}" -gt 0 ]; then
-            NETWORKUP="-YES-"
-        else
-            NETWORKUP="-NO-"
-        fi
-    fi
-}
-
-
-# Wait for network readiness.
-# This avoids the binding of PlexConnect to the loopback address which may otherwise occur
-# if the bash script is called from a LaunchDaemon/plist file at boot time.
-
-CheckForNetwork
-while [ "${NETWORKUP}" != "-YES-" ]
+until ping -c 1 8.8.8.8 > /dev/null
 do
-    sleep 5
-    NETWORKUP=
-    CheckForNetwork
+  echo "No internet, waiting!"
 done
+echo "Yay internet"
 
+#change directory & launch plexconnect
 
-# Start PlexConnect
-./${APPNAME}
+cd /Applications/PlexConnect
+./PlexConnect.py
