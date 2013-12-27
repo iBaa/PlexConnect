44fc6da3708d7fb07b0705a339a522d7dd8e2870
wahlmanj <wahlman.j@gmail.com>
2013-12-27 09:51:25 -0600

    Update 6.1.3-6.1.5.bash

--- support/IOS_jailbreak/6.1.3-6.1.5.bash
+++ support/IOS_jailbreak/6.1.3-6.1.5.bash
@@ -1,15 +1,17 @@
 #!/bin/bash
 
 #
-# OSX PlexConnect startup script
+# OSX PlexConnect startup script thanks to @stonegray on kiwiirc
 #
 
-#!/bin/bash thanks to @stonegray on kiwiirc
-until ping -t 100 -c 1 8.8.8.8 > /dev/null
+#!/bin/bash
+ping -W 100 -t 100 -c 1 8.8.8.8 > /dev/null 
 do
   echo "No internet, waiting!"
 done
 echo "Yay internet"
+
+#change directory & launch plexconnect
 
 cd /Applications/PlexConnect
 ./PlexConnect.py
