7fa0c29ab4ccb8947aa1f85ff321f8223277e491
wahlmanj <wahlman.j@gmail.com>
2013-12-27 12:21:11 -0600

    Update 7.bash

--- support/IOS_jailbreak/7.bash
+++ support/IOS_jailbreak/7.bash
@@ -4,8 +4,7 @@
 # OSX PlexConnect startup script thanks to @stonegray on #kiwiirc
 #
 
-echo "Can I has internet?"
-until ping -c 1 8.8.8.8 > /dev/null  #fixed?
+until ping -c 1 8.8.8.8 > /dev/null
 do
   echo "No internet, waiting!"
 done
