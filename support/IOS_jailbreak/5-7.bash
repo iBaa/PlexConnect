23f692141abbf2882a0610694b179ae8e023d578
wahlmanj <wahlman.j@gmail.com>
2013-12-28 00:40:43 -0600

    Create 5-7.bash

---
+++ support/IOS_jailbreak/5-7.bash
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#
+# OSX PlexConnect startup script
+#
+
+# Run in a loop until successfully connected to the internet
+until wget -q -O - http://www.google.com | grep Lucky > /dev/null; do
+sleep 10
+done
+exec $1&
+
+# Change directory & launch plexconnect
+cd /Applications/PlexConnect
+./PlexConnect.py 
