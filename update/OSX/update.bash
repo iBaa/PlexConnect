3260c656858354c8ed481509a5d62bb5c5fd0518
wahlmanj <wahlman.j@gmail.com>
2014-01-16 15:02:39 -0600

    Update update.bash

--- update/OSX/update.bash
+++ update/OSX/update.bash
@@ -5,7 +5,7 @@ cd "$( cd "$( dirname "$0" )" && pwd )"
 InstallerPath=${PWD}
 
 ## find PlexConnect main path
-cd ../..
+cd __INSTALLERPATH__
 PlexConnectPath=${PWD}
 
 ## change permissions of .git so update.bash can be ran without su
