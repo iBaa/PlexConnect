c916c8459b0b8ba19a9223fe8bf1759e503e079c
wahlmanj <wahlman.j@gmail.com>
2014-01-16 14:39:22 -0600

    Update install.bash

--- update/OSX/install.bash
+++ update/OSX/install.bash
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-## check for git and install if needed
-git
 
 ## define update.bash as executable
 chmod +x update.bash
@@ -9,12 +7,8 @@ chmod +x update.bash
 ## copy update bash so it can be ran without permissions
 cp update.bash /usr/bin
 
-## cd to PlexConnect directory
-cd ..
-cd ..
-
-## change permissions of .git so update.bash can be ran without su
-chown -R $USER .git
+## check for git and install if needed
+git
 
 ## warn user to install git prior to updates
 echo IF YOU CANCELED THE INSTALLATION OF GIT RERUN THIS SCIPT. DO NOT CONTINUE UNTIL GIT IS INSTALLED YOU HAVE BEEN WARNED!
