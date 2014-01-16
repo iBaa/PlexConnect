8fe906e85924a77768d9075799279e3dad78d028
wahlmanj <wahlman.j@gmail.com>
2014-01-16 16:05:49 -0600

    Update install.bash

--- update/OSX/install.bash
+++ update/OSX/install.bash
@@ -10,6 +10,8 @@ User=${USER}
 cd ../..
 PlexConnectPath=${PWD}
 
+
+
 ## replace __INSTALLERPATH__ in default update.bash
 ## save directly to the /usr/bin folder
 sed -e "s/__INSTALLERPATH__/${InstallerPath//\//\\/}/" "${InstallerPath}/update.bash" > /usr/bin/update.bash
@@ -25,5 +27,5 @@ chmod +x /usr/bin/update.bash
 git
 
 ## warn user to install git prior to updates
-echo IF YOU CANCELED THE INSTALLATION OF GIT RERUN THIS SCIPT. DO NOT CONTINUE UNTIL GIT IS INSTALLED YOU HAVE BEEN WARNED!
+echo IF YOU CANCELED THE INSTALLATION OF GIT RERUN THIS SCRIPT. DO NOT CONTINUE UNTIL GIT IS INSTALLED YOU HAVE BEEN WARNED!
 echo PROCEED ONLY IF YOU INSTALLED GIT PRIOR
