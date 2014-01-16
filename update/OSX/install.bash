8efbf954ccc1a4e8dada434ca0141fe47156daf0
wahlmanj <wahlman.j@gmail.com>
2014-01-16 10:12:48 -0600

    Update install.bash

--- update/OSX/install.bash
+++ update/OSX/install.bash
@@ -1,4 +1,16 @@
 #!/bin/bash
 
+## check for git and install if needed
+git
+
+## define update.bash as executable
 chmod +x update.bash
+
+## copy update bash so it can be ran without permissions
 cp update.bash /usr/bin
+
+## cd to PlexConnect directory
+cd "$( cd "$( dirname "$0" )" && pwd )"/../..
+
+## change permissions of .git so update.bash can be ran without su
+chown -R $USER .git
