5a1030845d5140770cdafcd3b235a0d0948ae166
wahlmanj <wahlman.j@gmail.com>
2014-01-16 12:15:30 -0600

    Update install.bash

--- update/OSX/install.bash
+++ update/OSX/install.bash
@@ -14,3 +14,7 @@ cd "$( cd "$( dirname "$0" )" && pwd )"/../..
 
 ## change permissions of .git so update.bash can be ran without su
 chown -R $USER .git
+
+## warn user to install git prior to updates
+echo IF YOU CANCELED THE INSTALLATION OF GIT RERUN THIS SCIPT. DO NOT CONTINUE UNTIL GIT IS INSTALLED YOU HAVE BEEN WARNED!
+echo PROCEED ONLY IF YOU INSTALLED GIT PRIOR
