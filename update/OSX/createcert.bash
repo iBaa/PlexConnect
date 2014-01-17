7d39de957a289f3beedc6c9f6d3ace8377cbe368
wahlmanj <wahlman.j@gmail.com>
2014-01-17 13:55:27 -0600

    Create createcert.bash

---
+++ update/OSX/createcert.bash
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+## cd to PlexConnect directory
+cd __INSTALLERPATH__
+cd ../..
+
+## Trailers - hostname is trailers.apple.com
+## certificate good for 10 years
+openssl req -new -nodes -newkey rsa:2048 -out ./assets/certificates/trailers.pem -keyout ./assets/certificates/trailers.key -x509 -days 3650 -subj "/C=US/CN=trailers.apple.com"
+openssl x509 -in ./assets/certificates/trailers.pem -outform der -out ./assets/certificates/trailers.cer && cat ./assets/certificates/trailers.key >> ./assets/certificates/trailers.pem
