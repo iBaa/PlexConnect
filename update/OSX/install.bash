136a7c9c6c2324b7b1b7a297c70aab250d4b6dbc
wahlmanj <wahlman.j@gmail.com>
2014-01-17 23:04:50 -0600

    Update install.bash

--- update/OSX/install.bash
+++ update/OSX/install.bash
@@ -4,6 +4,10 @@
 cd "$( cd "$( dirname "$0" )" && pwd )"
 InstallerPath=${PWD}
 
+## find PlexConnect main path
+cd ../..
+PlexConnectPath=${PWD}
+
 ## copy update.bash, createcert.bash, createplist.bash, com.plex.plexconnect.bash.plist to /usr.bin
 cp update.bash /usr/bin
 cp createcert.bash /usr/bin
@@ -12,14 +16,15 @@ cp com.plex.plexconnect.bash.plist /usr/bin
 
 ## replace __INSTALLERPATH__, __USERNAME__in default update.bash
 ## save directly to the /usr/bin folder
-
 sed -e "s/__INSTALLERPATH__/${InstallerPath//\//\\/}/;s/__USERNAME__/${SUDO_USER}/" "${InstallerPath}/update.bash" > /usr/bin/update.bash
 
 sed -e "s/__INSTALLERPATH__/${InstallerPath//\//\\/}/" "${InstallerPath}/createcert.bash" > /usr/bin/createcert.bash
 
 sed -e "s/__DEFAULTPATH__/${InstallerPath//\//\\/}/" "${InstallerPath}/createplist.bash" > /usr/bin/createplist.bash
 
-sed -e "s/__DEFAULTPATH__/${InstallerPath//\//\\/}/" "${InstallerPath}/com.plex.plexconnect.bash.plist" > /usr/bin/com.plex.plexconnect.bash.plist
+## replace __INSTALLERPATH__, __PLEXCONNECTPATH__ in default com.plex.plexconnect.daemon.bash.plist
+## save directly to the /Library/LaunchDameons folder
+sed -e "s/__INSTALLERPATH__/${InstallerPath//\//\\/}/;s/__PLEXCONNECTPATH__/${PlexConnectPath//\//\\/}/" "${InstallerPath}/com.plex.plexconnect.bash.plist" > /Library/LaunchDaemons/com.plex.plexconnect.bash.plist
 
 ## fix permissions
 chmod +x /usr/bin/update.bash
