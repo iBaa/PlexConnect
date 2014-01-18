f5794343ee8c37dcfac1fa7dedce53c561597f62
wahlmanj <wahlman.j@gmail.com>
2014-01-17 20:49:45 -0600

    Create update2.bash

---
+++ update/OSX/update2.bash
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+## save path to installer files
+cd "$( cd "$( dirname "$0" )" && pwd )"
+InstallerPath=${PWD}
+
+## update status
+echo 'Stopping PlexConnect...'
+
+## unload plexconnect from launchctl to avoid database errors
+cd /library/launchdaemons
+launchctl unload com.plex.plexconnect.plist
+
+## find PlexConnect main path
+cd __INSTALLERPATH__
+cd ../..
+
+## change permissions of .git so update.bash can be ran without su
+chown -R __USERNAME__ .git
+
+## update status
+echo 'Updating PlexConnect...'
+
+## get update
+git pull
+
+## wait a couple seconds to allow PlexConnect to update
+sleep 2
+
+## update status
+echo 'Starting PlexConnect...'
+
+## load plexconnect into launchctl after completed update
+cd /library/launchdaemons
+launchctl load com.plex.plexconnect.plist
+
+## wait a couple seconds to allow PlexConnect to load
+sleep 2
+
+## display the running status of PlexConnect
+launchctl list | grep com.plex.plexconnect
