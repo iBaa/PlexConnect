5c3192187041797975ff6d47bdd2635eff6c778f
wahlmanj <wahlman.j@gmail.com>
2014-01-17 23:21:02 -0600

    Create createplist2.bash

---
+++ update/OSX/createplist2.bash
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+## create autostart plist for next boot
+echo 'Installing PlexConnect...'
+
+## change ownership and permissions of the plist file to make it launchctl compatible
+chown root /Library/LaunchDaemons/com.plex.plexconnect.plist
+chmod 644 /Library/LaunchDaemons/com.plex.plexconnect.plist
+
+## start PlexConnect for this session
+echo 'Starting PlexConnect...'
+
+## launch the plist so that we can use it without a reboot
+launchctl load /Library/LaunchDaemons/com.plex.plexconnect.plist
+
+## wait a couple seconds to allow PlexConnect to load
+sleep 2
+
+## display the running status of PlexConnect
+#./PlexConnect_daemon.bash status
+launchctl list | grep com.plex.plexconnect
