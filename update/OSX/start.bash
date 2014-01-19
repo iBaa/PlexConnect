3667f0dfabd093a8a3f6a05bc74c7857c95bbdd9
wahlmanj <wahlman.j@gmail.com>
2014-01-19 14:24:21 -0600

    Create start.bash

---
+++ update/OSX/start.bash
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+## update status
+echo 'Stopping PlexConnect...'
+
+## unload plexconnect from launchctl to avoid database errors
+cd /library/launchdaemons
+launchctl load com.plex.plexconnect.bash.plist
+
+## display the running status of PlexConnect
+launchctl list | grep com.plex.plexconnect.bash
