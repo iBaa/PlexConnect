3caa482306a2a4d66da37ad340f68500e183512e
wahlmanj <wahlman.j@gmail.com>
2014-01-19 14:21:05 -0600

    Update stop.bash

--- update/OSX/stop.bash
+++ update/OSX/stop.bash
@@ -7,9 +7,5 @@ echo 'Stopping PlexConnect...'
 cd /library/launchdaemons
 launchctl unload com.plex.plexconnect.bash.plist
 
-## find PlexConnect main path
-cd __INSTALLERPATH__
-cd ../..
-
 ## display the running status of PlexConnect
 launchctl list | grep com.plex.plexconnect.bash
