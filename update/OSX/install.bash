67fde8a8087b4946d1097b671e9a25dcc1f42b23
wahlmanj <wahlman.j@gmail.com>
2014-01-20 19:39:52 -0600

    Update install.bash

--- update/OSX/install.bash
+++ update/OSX/install.bash
@@ -23,6 +23,7 @@ cp start.bash /usr/bin
 cp start2.bash /usr/bin
 cp restart.bash /usr/bin
 cp restart2.bash /usr/bin
+cp webconnect.bash /usr/bin
 
 ## replace __INSTALLERPATH__ in default createcert.bash
 ## save directly to the /usr/bin folder
@@ -56,7 +57,7 @@ chmod +x /usr/bin/start.bash
 chmod +x /usr/bin/start2.bash
 chmod +x /usr/bin/restart.bash
 chmod +x /usr/bin/restart2.bash
-
+chmod +x /usr/bin/webconnect.bash
 ## check for git and install if needed
 git
 
