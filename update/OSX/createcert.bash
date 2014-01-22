e736c798e6d245f59511456ae8a13c2fdd13626b
wahlmanj <wahlman.j@gmail.com>
2014-01-22 10:44:06 -0600

    Update createcert.bash

--- update/OSX/createcert.bash
+++ update/OSX/createcert.bash
@@ -3,8 +3,17 @@
 ## cd to PlexConnect directory
 cd __INSTALLERPATH__
 cd ../..
+PlexConnectPath=${PWD}
 
-## Trailers - hostname is trailers.apple.com
-## certificate good for 10 years
+
+## check SSL certificate
+file="${PlexConnectPath}/assets/certificates/trailers.pem"
+if [ -f "$file" ]; then
+    echo 'SSL certificate '$file' found. Skipping' 
+
+else
+
 openssl req -new -nodes -newkey rsa:2048 -out ./assets/certificates/trailers.pem -keyout ./assets/certificates/trailers.key -x509 -days 3650 -subj "/C=US/CN=trailers.apple.com"
 openssl x509 -in ./assets/certificates/trailers.pem -outform der -out ./assets/certificates/trailers.cer && cat ./assets/certificates/trailers.key >> ./assets/certificates/trailers.pem
+
+fi
