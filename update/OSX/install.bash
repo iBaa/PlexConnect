6a78e9c4de84fee6cf9877d0ffc0a96ab088adbc
wahlmanj <wahlman.j@gmail.com>
2014-01-25 01:22:41 -0600

    Update install.bash

--- update/OSX/install.bash
+++ update/OSX/install.bash
@@ -14,15 +14,10 @@ cd update/OSX
 ## copy files to /usr.bin for system wide access
 cp createcert.bash /usr/bin
 cp createplist.bash /usr/bin
-cp createplist2.bash /usr/bin
 cp update.bash /usr/bin
-cp update2.bash /usr/bin
 cp stop.bash /usr/bin
-cp stop2.bash /usr/bin
 cp start.bash /usr/bin
-cp start2.bash /usr/bin
 cp restart.bash /usr/bin
-cp restart2.bash /usr/bin
 cp webconnect.bash /usr/bin
 
 ## replace __INSTALLERPATH__ in default createcert.bash
@@ -33,30 +28,17 @@ sed -e "s/__INSTALLERPATH__/${InstallerPath//\//\\/}/" "${InstallerPath}/createc
 ## save directly to the /usr/bin folder
 sed -e "s/__DEFAULTPATH__/${InstallerPath//\//\\/}/" "${InstallerPath}/createplist.bash" > /usr/bin/createplist.bash
 
-## replace __DEFAULTPATH__ in default createplist2.bash
-## save directly to the /usr/bin folder
-sed -e "s/__DEFAULTPATH__/${InstallerPath//\//\\/}/" "${InstallerPath}/createplist2.bash" > /usr/bin/createplist2.bash
-
 ## replace __INSTALLERPATH__, __USERNAME__in default update.bash
 ## save directly to the /usr/bin folder
 sed -e "s/__INSTALLERPATH__/${InstallerPath//\//\\/}/;s/__USERNAME__/${SUDO_USER}/" "${InstallerPath}/update.bash" > /usr/bin/update.bash
 
-## replace __INSTALLERPATH__, __USERNAME__in default update2.bash
-## save directly to the /usr/bin folder
-sed -e "s/__INSTALLERPATH__/${InstallerPath//\//\\/}/;s/__USERNAME__/${SUDO_USER}/" "${InstallerPath}/update2.bash" > /usr/bin/update2.bash
-
 ## fix permissions
 chmod +x /usr/bin/createcert.bash
 chmod +x /usr/bin/createplist.bash
-chmod +x /usr/bin/createplist2.bash
 chmod +x /usr/bin/update.bash
-chmod +x /usr/bin/update2.bash
 chmod +x /usr/bin/stop.bash
-chmod +x /usr/bin/stop2.bash
 chmod +x /usr/bin/start.bash
-chmod +x /usr/bin/start2.bash
 chmod +x /usr/bin/restart.bash
-chmod +x /usr/bin/restart2.bash
 chmod +x /usr/bin/webconnect.bash
 
 ## check for git and install if needed
