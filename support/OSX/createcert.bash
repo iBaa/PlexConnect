bf80682770753a257a9bf4566433536e2ba5e85c
Jeroen Jacobs <github@jeroenj.be>
2014-01-31 21:11:28 +0100

    Improves SSL certificate generation
    Export the certificate in the PEM format instead of the DER format so it does not need to be converted before merging in the pem file.

--- support/OSX/createcert.bash
+++ support/OSX/createcert.bash
@@ -5,5 +5,5 @@ cd "$( cd "$( dirname "$0" )" && pwd )"/../..
 
 ## Trailers - hostname is trailers.apple.com
 ## certificate good for 10 years
-openssl req -new -nodes -newkey rsa:2048 -out ./assets/certificates/trailers.pem -keyout ./assets/certificates/trailers.key -x509 -days 3650 -subj "/C=US/CN=trailers.apple.com"
-openssl x509 -in ./assets/certificates/trailers.pem -outform der -out ./assets/certificates/trailers.cer && cat ./assets/certificates/trailers.key >> ./assets/certificates/trailers.pem
+openssl req -new -nodes -newkey rsa:2048 -outform pem -out ./assets/certificates/trailers.cer -keyout ./assets/certificates/trailers.key -x509 -days 3650 -subj "/C=US/CN=trailers.apple.com"
+cat ./assets/certificates/trailers.cer ./assets/certificates/trailers.key >> ./assets/certificates/trailers.pem
