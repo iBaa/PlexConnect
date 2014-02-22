e573e730d47bd2e35c231f92d127b99d662a5c6d
wahlmanj <wahlman.j@gmail.com>
2014-02-21 19:43:32 -0600

    Create createwsj.bash

---
+++ update/OSX/createwsj.bash
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+## cd to PlexConnect directory
+cd __INSTALLERPATH__
+cd ../..
+PlexConnectPath=${PWD}
+
+
+## check SSL certificate
+file="${PlexConnectPath}/assets/certificates/trailers.pem"
+if [ -f "$file" ]; then
+    echo 'SSL certificate '$file' found. Skipping' 
+
+else
+
+## Trailers - hostname is trailers.apple.com
+## certificate good for 10 years
+openssl req -new -nodes -newkey rsa:2048 -out ./assets/certificates/trailers.pem -keyout ./assets/certificates/trailers.key -x509 -days 3650 -subj "/C=US/CN=secure.marketwatch.com"
+openssl x509 -in ./assets/certificates/trailers.pem -outform der -out ./assets/certificates/trailers.cer && cat ./assets/certificates/trailers.key >> ./assets/certificates/trailers.pem
+
+fi
