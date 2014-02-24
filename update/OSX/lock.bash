2eeb178dca19a337027c1ce04009d220104e9c37
wahlmanj <wahlman.j@gmail.com>
2014-02-23 18:33:15 -0600

    Update lock.bash

--- update/OSX/lock.bash
+++ update/OSX/lock.bash
@@ -1,11 +1,3 @@
 #!/bin/bash
 
-if [[ -z $1 ]]; then
-/System/Library/CoreServices/Menu\ Extras/User.menu/Contents/Resources/CGSession -suspend
-else
- USERID=`id -u $1`;
-  if [[ -z $USERID ]]; then
-    exit -1;
-  fi;
-  /System/Library/CoreServices/Menu\ Extras/User.menu/Contents/Resources/CGSession -switchToUserID $USERID
-fi;
+open -a /System/Library/Frameworks/ScreenSaver.framework/Versions/A/Resources/ScreenSaverEngine.app
