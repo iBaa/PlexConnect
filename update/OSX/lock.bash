cc4f77e93fab1592421b5b0b4ccf9e9b75683bbd
wahlmanj <wahlman.j@gmail.com>
2014-02-23 18:29:44 -0600

    Update lock.bash

--- update/OSX/lock.bash
+++ update/OSX/lock.bash
@@ -3,4 +3,9 @@
 if [[ -z $1 ]]; then
 /System/Library/CoreServices/Menu\ Extras/User.menu/Contents/Resources/CGSession -suspend
 else
-
+ USERID=`id -u $1`;
+  if [[ -z $USERID ]]; then
+    exit -1;
+  fi;
+  /System/Library/CoreServices/Menu\ Extras/User.menu/Contents/Resources/CGSession -switchToUserID $USERID
+fi;
