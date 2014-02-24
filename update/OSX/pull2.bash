7bfac1c275fd6c11633125be4ece5e88e4257cdd
wahlmanj <wahlman.j@gmail.com>
2014-02-24 15:32:34 -0600

    Update pull2.bash

--- update/OSX/pull2.bash
+++ update/OSX/pull2.bash
@@ -1,13 +1,7 @@
 #!/bin/bash
 
-export PATH=$PATH:/usr/local/git/bin/
-if [ -d "/applications/onlytemp" ]
-then
-    echo "Directory /path/to/dir exists."
-else
 mkdir /Applications/onlytemp
 cd /applications/onlytemp
 git clone https://github.com/wahlmanj/PlexConnect.git
 cp -R /applications/onlytemp/plexconnect/update/osx/* /applications/plexconnect/update/osx
 rm -R /applications/onlytemp
-fi
