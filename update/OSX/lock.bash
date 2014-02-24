d2655b38c0759f4b1d683d4b83b6a0bc2cb4c356
wahlmanj <wahlman.j@gmail.com>
2014-02-23 18:22:54 -0600

    Update lock.bash

--- update/OSX/lock.bash
+++ update/OSX/lock.bash
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-  /System/Library/CoreServices/Menu\ Extras/User.menu/Contents/Resources/CGSession -suspend
+/System/Library/CoreServices/Menu\ Extras/User.menu/Contents/Resources/CGSession -suspend
