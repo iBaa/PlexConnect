f0c7f618de1abd20e80d652ac7e5e6fff8bf7121
wahlmanj <wahlman.j@gmail.com>
2014-02-23 19:12:52 -0600

    Update webconnect.bash

--- update/OSX/webconnect.bash
+++ update/OSX/webconnect.bash
@@ -27,6 +27,7 @@ cp restartbash.bash /usr/bin
 cp statusbash.bash /usr/bin
 cp rebootbash.bash /usr/bin
 cp lockbash.bash /usr/bin
+cp trashbash.bash /usr/bin
 cp sudoers /etc
 cp httpd.conf /etc/apache2
 
@@ -42,6 +43,7 @@ chown root /usr/bin/restartbash.bash
 chown root /usr/bin/statusbash.bash
 chown root /usr/bin/rebootbash.bash
 chown root /usr/bin/lockbash.bash
+chown root /usr/bin/trashbash.bash
 
 chmod +x /usr/bin/removecertsbash.bash
 chmod +x /usr/bin/createcertbash.bash
@@ -55,6 +57,7 @@ chmod +x /usr/bin/restartbash.bash
 chmod +x /usr/bin/statusbash.bash
 chmod +x /usr/bin/rebootbash.bash
 chmod +x /usr/bin/lockbash.bash
+chmod +x /usr/bin/trashbash.bash
 
 chmod 4755 /usr/bin/removecertsbash.bash
 chmod 4755 /usr/bin/createcertbash.bash
@@ -68,6 +71,7 @@ chmod 4755 /usr/bin/restartbash.bash
 chmod 4755 /usr/bin/statusbash.bash
 chmod 4755 /usr/bin/rebootbash.bash
 chmod 4755 /usr/bin/lockbash.bash
+chmod 4755 /usr/bin/trashbash.bash
 chmod 400 /etc/sudoers
 
 ## warn user to use the visudo command to edit sudoers
