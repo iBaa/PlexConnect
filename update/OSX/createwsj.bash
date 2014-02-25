cac47cd5394c98f6cebfacc1e51feb8714de9b4f
wahlmanj <wahlman.j@gmail.com>
2014-02-24 20:36:07 -0600

    Update createwsj.bash

--- update/OSX/createwsj.bash
+++ update/OSX/createwsj.bash
@@ -10,7 +10,7 @@ PlexConnectPath=${PWD}
 file="${PlexConnectPath}/assets/certificates/trailers.pem"
 if [ -f "$file" ]; then
     echo 'SSL certificate '$file' found. Skipping' 
-
+    echo 'Settings.cfg changed to hijack secure.marketwatch.com'
 else
 
 ## Trailers - hostname is trailers.apple.com
