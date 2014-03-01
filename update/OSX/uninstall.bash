17cc2de47b84ff803216a9006b01f4d7ff6044ee
wahlmanj <wahlman.j@gmail.com>
2014-03-01 13:33:42 -0600

    Update uninstall.bash

--- update/OSX/uninstall.bash
+++ update/OSX/uninstall.bash
@@ -11,34 +11,34 @@ rm /Library/Launchdaemons/com.plex.plexconnect.bash.plist
 rm /Library/WebServer/CGI-Executables/bash.cgi
 rm /usr/bin/createcert.bash
 rm /usr/bin/createimovie.bash 
-rm /usr/bin/createwsj.bash /usr/bin
-rm /usr/bin/createplist.bash /usr/bin
-rm /usr/bin/createauto.bash /usr/bin
-rm /usr/bin/update.bash /usr/bin
-rm /usr/bin/stop.bash /usr/bin
-rm /usr/bin/start.bash /usr/bin
-rm /usr/bin/restart.bash /usr/bin
-rm /usr/bin/status.bash /usr/bin
-rm /usr/bin/reboot.bash /usr/bin
-rm /usr/bin/removecerts.bash /usr/bin
-rm /usr/bin/lock.bash /usr/bin
-rm /usr/bin/trash.bash /usr/bin
-rm /usr/bin/webconnect.bash /usr/bin
-rm /usr/bin/updatewc.bash /usr/bin
-rm /usr/bin/pull.bash /usr/bin
-rm /usr/bin/pull2.bash /usr/bin
-rm /usr/bin/removecertsbash.bash /usr/bin
-rm /usr/bin/createcertbash.bash /usr/bin
-rm /usr/bin/createimoviebash.bash /usr/bin
-rm /usr/bin/createwsjbash.bash /usr/bin
-rm /usr/bin/createplistbash.bash /usr/bin
-rm /usr/bin/updatebash.bash /usr/bin
+rm /usr/bin/createwsj.bash
+rm /usr/bin/createplist.bash
+rm /usr/bin/createauto.bash
+rm /usr/bin/update.bash
+rm /usr/bin/stop.bash
+rm /usr/bin/start.bash
+rm /usr/bin/restart.bash
+rm /usr/bin/status.bash
+rm /usr/bin/reboot.bash
+rm /usr/bin/removecerts.bash
+rm /usr/bin/lock.bash
+rm /usr/bin/trash.bash
+rm /usr/bin/webconnect.bash
+rm /usr/bin/updatewc.bash
+rm /usr/bin/pull.bash
+rm /usr/bin/pull2.bash
+rm /usr/bin/removecertsbash.bash
+rm /usr/bin/createcertbash.bash
+rm /usr/bin/createimoviebash.bash
+rm /usr/bin/createwsjbash.bash
+rm /usr/bin/createplistbash.bash
+rm /usr/bin/updatebash.bash
 rm /usr/bin/stopbash.bash /usr/bin
 rm /usr/bin/startbash.bash /usr/bin
 rm /usr/bin/restartbash.bash /usr/bin
 rm /usr/bin/statusbash.bash /usr/bin
 rm /usr/bin/rebootbash.bash /usr/bin
-rm /usr/bin/lockbash.bash /usr/bin
-rm /usr/bin/trashbash.bash /usr/bin
-rm /usr/bin/updatewcbash.bash /usr/bin
+rm /usr/bin/lockbash.bas
+rm /usr/bin/trashbash.bash
+rm /usr/bin/updatewcbash.bash
 rm -Rf /Applications/PlexConnect
