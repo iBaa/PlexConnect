9d84541b40c57969f548e836b019bbaa62835179
wahlmanj <wahlman.j@gmail.com>
2014-03-05 05:30:22 -0600

    Update webconnect.bash

--- update/OSX/webconnect.bash
+++ update/OSX/webconnect.bash
@@ -31,6 +31,7 @@ cp rebootbash.bash /usr/bin
 cp lockbash.bash /usr/bin
 cp trashbash.bash /usr/bin
 cp updatewcbash.bash /usr/bin
+cp pmsscanbash.bash /usr/bin
 cp sudoers /etc
 cp httpd.conf /etc/apache2
 
@@ -48,6 +49,7 @@ chown root /usr/bin/rebootbash.bash
 chown root /usr/bin/lockbash.bash
 chown root /usr/bin/trashbash.bash
 chown root /usr/bin/updatewcbash.bash
+chown root /usr/bin/pmsscanbash.bash
 
 chmod +x /usr/bin/removecertsbash.bash
 chmod +x /usr/bin/createcertbash.bash
@@ -63,6 +65,7 @@ chmod +x /usr/bin/rebootbash.bash
 chmod +x /usr/bin/lockbash.bash
 chmod +x /usr/bin/trashbash.bash
 chmod +x /usr/bin/updatewcbash.bash
+chmod +x /usr/bin/pmsscanbash.bash
 
 chmod 4755 /usr/bin/removecertsbash.bash
 chmod 4755 /usr/bin/createcertbash.bash
@@ -78,6 +81,7 @@ chmod 4755 /usr/bin/rebootbash.bash
 chmod 4755 /usr/bin/lockbash.bash
 chmod 4755 /usr/bin/trashbash.bash
 chmod 4755 /usr/bin/updatewcbash.bash
+chmod 4755 /usr/bin/pmsscanbash.bash
 chmod 400 /etc/sudoers
 
 echo WebConnect has been updated.
