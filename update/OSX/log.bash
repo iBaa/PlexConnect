cb5cf9dfe83f5dfce8446170847268eb31ed506c
wahlmanj <wahlman.j@gmail.com>
2014-03-11 02:56:47 -0500

    Update log.bash

--- update/OSX/log.bash
+++ update/OSX/log.bash
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-echo 'This command displays your plexconnect log file'
-
 ## Display PlexConnect log
 FILE="/applications/plexconnect/plexconnect.log"
 echo "*** File - $FILE contents ***"
