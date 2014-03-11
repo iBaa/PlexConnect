e25ea1db43842d3d577c217030ddda59d422c2e6
wahlmanj <wahlman.j@gmail.com>
2014-03-11 03:04:39 -0500

    Update start.bash

--- update/OSX/start.bash
+++ update/OSX/start.bash
@@ -9,8 +9,3 @@ launchctl load com.plex.plexconnect.bash.plist
 
 ## display the running status of PlexConnect
 launchctl list | grep com.plex.plexconnect.bash
-
-## Display PlexConnect log
-FILE="/applications/plexconnect/plexconnect.log"
-echo "*** File - $FILE contents ***"
-cat $FILE
