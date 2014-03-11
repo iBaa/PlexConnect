f81472e6d7578690f74a3a6f69c13a799a965ac2
wahlmanj <wahlman.j@gmail.com>
2014-03-11 03:23:04 -0500

    Update restart.bash

--- update/OSX/restart.bash
+++ update/OSX/restart.bash
@@ -16,3 +16,8 @@ sleep 1
 ## display the running status of PlexConnect
 #./PlexConnect_daemon.bash status
 launchctl list | grep com.plex.plexconnect.bash
+
+## Display PlexConnect log
+FILE="/applications/plexconnect/plexconnect.log"
+echo "*** File - $FILE contents ***"
+cat $FILE
