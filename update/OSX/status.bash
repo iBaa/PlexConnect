783cb31a8045636dd1ba2e37a683a17647d3ae86
wahlmanj <wahlman.j@gmail.com>
2014-03-11 03:44:28 -0500

    Update status.bash

--- update/OSX/status.bash
+++ update/OSX/status.bash
@@ -3,3 +3,12 @@
 ## display the running status of PlexConnect
 ps axo pid,command | grep -i plexconnect
 
+## Display PlexConnect log
+FILE="/applications/plexconnect/plexconnect.log"
+echo "*** File - $FILE contents ***"
+cat $FILE
+
+## Display Settings.cfg
+FILE="/applications/plexconnect/settings.cfg"
+echo "*** File - $FILE contents ***"
+cat $FILE
