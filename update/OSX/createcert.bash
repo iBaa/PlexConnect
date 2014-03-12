6854dd2844ed259ca900bc06333237b22091583f
wahlmanj <wahlman.j@gmail.com>
2014-03-12 11:19:06 -0500

    Update createcert.bash

--- update/OSX/createcert.bash
+++ update/OSX/createcert.bash
@@ -5,8 +5,8 @@ cd __INSTALLERPATH__
 cd ../..
 PlexConnectPath=${PWD}
 
-rm -Rf /applications/plexconnect/settings.cfg
-cp /Applications/plexconnect/update/OSX/trailers/settings.cfg /applications/plexconnect
+sed -i '' 's/secure.marketwatch.com/trailers.apple.com/g' settings.cfg
+sed -i '' 's/www.icloud.com/trailers.apple.com/g' settings.cfg
 
 ## check SSL certificate
 file="${PlexConnectPath}/assets/certificates/trailers.pem"
@@ -22,8 +22,8 @@ else
 openssl req -new -nodes -newkey rsa:2048 -out ./assets/certificates/trailers.pem -keyout ./assets/certificates/trailers.key -x509 -days 3650 -subj "/C=US/CN=trailers.apple.com"
 openssl x509 -in ./assets/certificates/trailers.pem -outform der -out ./assets/certificates/trailers.cer && cat ./assets/certificates/trailers.key >> ./assets/certificates/trailers.pem
 
-rm -Rf /applications/plexconnect/settings.cfg
-cp /Applications/plexconnect/update/OSX/trailers/settings.cfg /applications/plexconnect
+sed -i '' 's/secure.marketwatch.com/trailers.apple.com/g' settings.cfg
+sed -i '' 's/www.icloud.com/trailers.apple.com/g' settings.cfg
 
 echo 'Generating Trailers certs'
 echo 'Settings.cfg changed to hijack trailers.apple.com'
