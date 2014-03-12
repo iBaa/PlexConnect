325ff2d31bc86a779caff8a5fae59bc06de77409
wahlmanj <wahlman.j@gmail.com>
2014-03-12 11:20:14 -0500

    Update createimovie.bash

--- update/OSX/createimovie.bash
+++ update/OSX/createimovie.bash
@@ -6,8 +6,8 @@ cd ../..
 PlexConnectPath=${PWD}
 
 
-rm -Rf /applications/plexconnect/settings.cfg
-cp /Applications/plexconnect/update/OSX/imovie/settings.cfg /applications/plexconnect
+sed -i '' 's/trailers.apple.com/www.icloud.com/g' settings.cfg
+sed -i '' 's/secure.marketwatch.com/www.icloud.com/g' settings.cfg
 
 ## check SSL certificate
 file="${PlexConnectPath}/assets/certificates/trailers.pem"
@@ -22,8 +22,8 @@ else
 openssl req -new -nodes -newkey rsa:2048 -out ./assets/certificates/trailers.pem -keyout ./assets/certificates/trailers.key -x509 -days 3650 -subj "/C=US/CN=www.icloud.com"
 openssl x509 -in ./assets/certificates/trailers.pem -outform der -out ./assets/certificates/trailers.cer && cat ./assets/certificates/trailers.key >> ./assets/certificates/trailers.pem
 
-rm -Rf /applications/plexconnect/settings.cfg
-cp /Applications/plexconnect/update/OSX/imovie/settings.cfg /applications/plexconnect
+sed -i '' 's/trailers.apple.com/www.icloud.com/g' settings.cfg
+sed -i '' 's/secure.marketwatch.com/www.icloud.com/g' settings.cfg
 
 echo 'Generating Imovie certs'
 echo 'Settings.cfg changed to hijack www.icloud.com'
