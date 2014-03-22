6176ac1dea76e6b55aa84ce6e7f14970436330ad
wahlmanj <wahlman.j@gmail.com>
2014-03-21 22:47:45 -0500

    Update createcert.bash

--- update/OSX/createcert.bash
+++ update/OSX/createcert.bash
@@ -5,8 +5,9 @@ cd __INSTALLERPATH__
 cd ../..
 PlexConnectPath=${PWD}
 
-sed -i '' 's/secure.marketwatch.com/trailers.apple.com/g' settings.cfg
+
 sed -i '' 's/www.icloud.com/trailers.apple.com/g' settings.cfg
+sed -i '' 's/secure.marketwatch.com/trailers.apple.com/g' settings.cfg
 
 ## check SSL certificate
 file="${PlexConnectPath}/assets/certificates/trailers.pem"
@@ -14,20 +15,19 @@ if [ -f "$file" ]; then
     echo 'SSL certificate '$file' found. Skipping' 
     echo 'Settings.cfg changed to hijack trailers.apple.com'
     echo 'Upload profile to ATV using this url http://trailers.apple.com/trailers.cer'
-
 else
 
 ## Trailers - hostname is trailers.apple.com
 ## certificate good for 10 years
-openssl req -new -nodes -newkey rsa:2048 -out ./assets/certificates/trailers.pem -keyout ./assets/certificates/trailers.key -x509 -days 3650 -subj "/C=US/CN=trailers.apple.com"
+openssl req -new -nodes -newkey rsa:2048 -out ./assets/certificates/trailers.pem -keyout ./assets/certificates/trailers.key -x509 -days 3650 -subj "/C=US/CN=Trailers.apple.com"
 openssl x509 -in ./assets/certificates/trailers.pem -outform der -out ./assets/certificates/trailers.cer && cat ./assets/certificates/trailers.key >> ./assets/certificates/trailers.pem
 
-sed -i '' 's/secure.marketwatch.com/trailers.apple.com/g' settings.cfg
 sed -i '' 's/www.icloud.com/trailers.apple.com/g' settings.cfg
+sed -i '' 's/secure.marketwatch.com/trailers.apple.com/g' settings.cfg
 
 echo 'Generating Trailers certs'
 echo 'Settings.cfg changed to hijack trailers.apple.com'
-echo 'Upload profile to ATV using this url http://trailers.apple.com/trailers.cer'
+echo 'Upload profile to ATV using this url http://www.icloud.com/trailers.cer'
 restart.bash
 
 fi
