67f3a8470f7d1b27247bc54fc12098a849fbf450
wahlmanj <wahlman.j@gmail.com>
2014-03-21 22:37:01 -0500

    Update createwsj.bash

--- update/OSX/createwsj.bash
+++ update/OSX/createwsj.bash
@@ -21,8 +21,6 @@ else
 openssl req -new -nodes -newkey rsa:2048 -out ./assets/certificates/trailers.pem -keyout ./assets/certificates/trailers.key -x509 -days 3650 -subj "/C=US/CN=secure.marketwatch.com"
 openssl x509 -in ./assets/certificates/trailers.pem -outform der -out ./assets/certificates/trailers.cer && cat ./assets/certificates/trailers.key >> ./assets/certificates/trailers.pem
 
-fi
-
 sed -i '' 's/trailers.apple.com/secure.marketwatch.com/g' settings.cfg
 sed -i '' 's/www.icloud.com/secure.marketwatch.com/g' settings.cfg
 
