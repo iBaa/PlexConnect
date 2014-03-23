7e2acf9b8b931b54a2813f6debb3208e1b811aca
wahlmanj <wahlman.j@gmail.com>
2014-03-23 14:18:14 -0500

    Update updatetest.bash

--- update/OSX/updatetest.bash
+++ update/OSX/updatetest.bash
@@ -1,5 +1,6 @@
 #!/bin/bash
-cd /applications/plexconnect
+
+export PATH=$PATH:/usr/local/git/bin/
  
 # fetch changes, git stores them in FETCH_HEAD
 git fetch
@@ -8,18 +9,10 @@ git fetch
 newUpdatesAvailable=`git diff HEAD FETCH_HEAD`
 if [ "$newUpdatesAvailable" != "" ]
 then
-        # create the fallback
-        git branch fallbacks
-        git checkout fallbacks
- 
-        git add .
-        git add -u
-        git commit -m `date "+%Y-%m-%d"`
-        echo "fallback created"
- 
-        git checkout master
-        git merge FETCH_HEAD
-        echo "merged updates"
+
+## get update
+git pull
+
 else
         echo "no updates available"
 fi
