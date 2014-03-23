d4db9cbb7f6a65c64d33a070bc01fdfd3c7f3f4e
wahlmanj <wahlman.j@gmail.com>
2014-03-23 05:11:22 -0500

    Update updatetest.bash

--- update/OSX/updatetest.bash
+++ update/OSX/updatetest.bash
@@ -1,13 +1,7 @@
 #!/bin/bash
 
-START_HERE="/applications/plexconnect";
-
-cd $START_HERE;
-
-for d in $(find . -maxdepth 1 -mindepth 1 -type d); do
-        echo -e "$d";
-        cd $d;
-        git remote -v;
-        git pull;
-        cd $START_HERE;
-done
+cd /applications/plexconnext
+git fetch origin
+reslog=$(git log HEAD..origin/master --oneline)
+if [[ "${reslog}" != "" ]] ; then
+git pull
