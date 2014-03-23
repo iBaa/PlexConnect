cca735d4c50c992986607af3984b93c43d961a82
wahlmanj <wahlman.j@gmail.com>
2014-03-23 14:09:14 -0500

    Update updatetest.bash

--- update/OSX/updatetest.bash
+++ update/OSX/updatetest.bash
@@ -1,9 +1,25 @@
 #!/bin/bash
 cd /applications/plexconnect
-if git fetch origin master &&
-    [ `git rev-list HEAD...origin/master --count` != 0 ] &&
+ 
+# fetch changes, git stores them in FETCH_HEAD
+git fetch
+ 
+# check for remote changes in origin repository
+newUpdatesAvailable=`git diff HEAD FETCH_HEAD`
+if [ "$newUpdatesAvailable" != "" ]
 then
-git pull
+        # create the fallback
+        git branch fallbacks
+        git checkout fallbacks
+ 
+        git add .
+        git add -u
+        git commit -m `date "+%Y-%m-%d"`
+        echo "fallback created"
+ 
+        git checkout master
+        git merge FETCH_HEAD
+        echo "merged updates"
 else
-    echo 'Not updated.'
+        echo "no updates available"
 fi
