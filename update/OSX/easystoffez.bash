d6af80344211cb821dab31102e7d8f76d99e1a00
wahlmanj <wahlman.j@gmail.com>
2014-04-04 10:55:36 -0500

    Update easystoffez.bash

--- update/OSX/easystoffez.bash
+++ update/OSX/easystoffez.bash
@@ -3,5 +3,8 @@
 ## navigate to update folder
 cd /applications/plexconnect/update/osx
 
+cp websharing.bash /usr.bin
+chmod +x /usr/bin websharing.bash
+
 cp -R Gradient /library/webserver/documents
 chmod 777 /library/webserver/documents/gradient/ *.*
