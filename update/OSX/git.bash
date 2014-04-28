5b71d940feedde1c404b090c3aed1502a5b5a8ff
wahlmanj <wahlman.j@gmail.com>
2014-04-28 17:32:09 -0500

    Delete git.bash

--- update/OSX/git.bash
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Check for brew and install it
-if [ ! -f "/usr/local/bin/brew" ]; then
-  ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)"
-  brew prune
-fi
-
-brew install git
