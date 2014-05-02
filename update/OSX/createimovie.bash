c426ab1c4eb9453b5cfb550faefd156304971e12
wahlmanj <wahlman.j@gmail.com>
2014-05-02 14:02:32 -0500

    Update createimovie.bash

--- update/OSX/createimovie.bash
+++ update/OSX/createimovie.bash
@@ -28,6 +28,5 @@ sed -i '' 's/secure.marketwatch.com/www.icloud.com/g' settings.cfg
 echo 'Generating Imovie certs'
 echo 'Settings.cfg changed to hijack www.icloud.com'
 echo 'Upload profile to ATV using this url http://www.icloud.com/trailers.cer'
-restart.bash
 
 fi
