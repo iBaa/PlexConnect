0a6fcbec702cf73bb62e799c7742ecee9f81bac0
wahlmanj <wahlman.j@gmail.com>
2014-05-02 14:03:17 -0500

    Update createcert.bash

--- update/OSX/createcert.bash
+++ update/OSX/createcert.bash
@@ -28,6 +28,5 @@ sed -i '' 's/secure.marketwatch.com/trailers.apple.com/g' settings.cfg
 echo 'Generating Trailers certs'
 echo 'Settings.cfg changed to hijack trailers.apple.com'
 echo 'Upload profile to ATV using this url http://trailers.apple.com/trailers.cer'
-restart.bash
 
 fi
