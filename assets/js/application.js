6a07236a71aa308a2cb2817dc1cb4df289e91ee8
Eric Yanush <ericyanush@gmail.com>
2014-05-24 16:38:08 -0600

    add screensaver handling functions to application.js

--- assets/js/application.js
+++ assets/js/application.js
@@ -279,6 +279,35 @@ function updateSubtitle(time)
 
 
 /*
+ * Trailers Movie poster screensaver handler functions
+ */
+atv.onScreensaverPhotosSelectionEntry = function() {
+    // The collection object is passed to atv.onExecuteQuery as parameters to load Images.
+    // Currently only one collection is able to be passed.
+    var collection = {
+        "id": "trailers-posters",
+        "name": "Trailer Posters",
+        "type": "collection"
+    };
+    atv.setScreensaverPhotosCollection(collection);
+}
+atv.onScreensaverPhotosSelectionExit = function() {
+}
+atv.onExecuteQuery = function(query, callback) {
+  var requestedNumImages = query.length;
+
+  var xhr = new XMLHttpRequest();
+  xhr.open("GET", "{{URL(/)}}" + "/ScreenSaver.json?PlexConnectUDID=" + atv.device.udid + "&length=" + requestedNumImages, false);
+  xhr.send();
+
+  var ScreensaverPhotos = JSON.parse(xhr.responseText);
+  if (ScreensaverPhotos.length > requestedNumImages) {
+    ScreensaverPhotos = ScreensaverPhotos.slice(0, requestedNumImages);
+  }
+  callback.success(ScreensaverPhotos);
+}
+
+/*
  *
  * Main app entry point
  *
