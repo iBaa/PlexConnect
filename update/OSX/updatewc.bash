12749726b21292cb5550ba8c3d1368a9b0d40cc3
wahlmanj <wahlman.j@gmail.com>
2014-06-11 21:48:36 -0300

    Update updatewc.bash

--- update/OSX/updatewc.bash
+++ update/OSX/updatewc.bash
@@ -2,12 +2,12 @@
 
 export PATH=$PATH:/usr/local/git/bin/
 mkdir /Applications/onlytemp
-cd /applications/onlytemp
+cd /Applications/onlytemp
 git clone https://github.com/wahlmanj/PlexConnect.git
-cp -R /applications/onlytemp/plexconnect/update/osx/* /applications/plexconnect/update/osx
-rm -R /applications/onlytemp
+cp -R /Applications/onlytemp/plexconnect/update/osx/* /Applications/PlexConnect/update/osx
+rm -R /Applications/onlytemp
 cd /
-cd /applications/plexconnect/update/osx
+cd /Applications/PlexConnect/update/osx
 cp installwc.bash /usr/bin
 chmod +x /usr/bin/installwc.bash
 installwc.bash
