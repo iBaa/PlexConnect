9e1a23a59a25aba51ab0bcf43831ae724e2378f1
wahlmanj <wahlman.j@gmail.com>
2014-06-11 23:35:54 -0500

    Update stop.bash

--- update/OSX/stop.bash
+++ update/OSX/stop.bash
@@ -11,6 +11,6 @@ launchctl unload com.plex.plexconnect.bash.plist
 launchctl list | grep com.plex.plexconnect.bash
 
 ## Display PlexConnect log
-FILE="/Applications/PlexConnect/plexconnect.log"
+FILE="/Applications/PlexConnect/PlexConnect.log"
 echo "*** File - $FILE contents ***"
 cat $FILE
