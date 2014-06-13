5fa523cc7f722e3207e59b1d1b00f2ef09fc6cbd
wahlmanj <wahlman.j@gmail.com>
2014-06-12 23:30:01 -0500

    Update sudoers.bash

--- update/OSX/sudoers.bash
+++ update/OSX/sudoers.bash
@@ -3,4 +3,4 @@ cd /Applications/PlexConnect/update/OSX
 #current user
 whoami=${USER}
 
-sed -e "s/__USERNAME__/$whoami/" "/Applications/Plexconnect/update/OSX/sudoers" > /Applications/Plexconnect/update/OSX/sudoers2
+sed -e "s/__USERNAME__/$whoami/" "/Applications/PlexConnect/update/OSX/sudoers" > /Applications/PlexConnect/update/OSX/sudoers2
