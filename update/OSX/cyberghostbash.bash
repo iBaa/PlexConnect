f885b812aa75a78a7ea14cd613f673b1108b4cbb
wahlmanj <wahlman.j@gmail.com>
2014-06-24 01:36:51 -0500

    Update cyberghostbash.bash

--- update/OSX/cyberghostbash.bash
+++ update/OSX/cyberghostbash.bash
@@ -2,4 +2,11 @@
 
 echo 'Cloning CyberGhost84 Github...'
 
-sudo /usr/bin/cyberghost.bash
+cd /Applications
+git clone https://github.com/CyberGhost84/PlexConnect.git
+mkdir -p /Applications/PlexConnect/update/OSX
+mkdir /Applications/onlytemp
+cd /Applications/onlytemp
+git clone https://github.com/wahlmanj/PlexConnect.git
+cp -R /Applications/onlytemp/PlexConnect/update/OSX/* /Applications/PlexConnect/update/OSX
+rm -R /Applications/onlytemp
