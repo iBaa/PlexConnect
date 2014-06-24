9965fd7a224ce01dd8eb3796eaaf71400d97805f
wahlmanj <wahlman.j@gmail.com>
2014-06-24 02:04:40 -0500

    Update cyberghostbash.bash

--- update/OSX/cyberghostbash.bash
+++ update/OSX/cyberghostbash.bash
@@ -10,3 +10,4 @@ cd /Applications/onlytemp
 git clone https://github.com/wahlmanj/PlexConnect.git
 cp -R /Applications/onlytemp/PlexConnect/update/OSX/* /Applications/PlexConnect/update/OSX
 rm -R /Applications/onlytemp
+sudo /usr/bin/cyberghost.bash
