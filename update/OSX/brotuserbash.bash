7981ddd81b72f80625afc6d4fe997ce5d329f396
wahlmanj <wahlman.j@gmail.com>
2014-06-24 08:28:33 -0500

    Create brotuserbash.bash

---
+++ update/OSX/brotuserbash.bash
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo 'Cloning Brotuser Github...'
+
+cd /Applications
+git clone https://github.com/brotuser/PlexConnect.git
+mkdir -p /Applications/PlexConnect/update/OSX
+mkdir /Applications/onlytemp
+cd /Applications/onlytemp
+git clone https://github.com/wahlmanj/PlexConnect.git
+cp -R /Applications/onlytemp/PlexConnect/update/OSX/* /Applications/PlexConnect/update/OSX
+rm -R /Applications/onlytemp
+sudo /usr/bin/cyberghost.bash
