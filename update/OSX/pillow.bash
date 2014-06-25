b08669001af47b18c306e02462c3d7986ea7f4df
wahlmanj <wahlman.j@gmail.com>
2014-06-25 17:26:26 -0500

    Update pillow.bash

--- update/OSX/pillow.bash
+++ update/OSX/pillow.bash
@@ -3,4 +3,6 @@
 cd /Applications/PlexConnect/update/OSX
 yes | ./pillow
 
+echo 'You need to choose a theme to install pillow'
+echo 'If you already installed a theme ignore this message'
 echo 'Close Terminal to continue installation...'
