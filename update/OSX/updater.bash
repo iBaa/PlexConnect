6010304f03d6c66b06f00179e1afc98a26be6d0e
wahlmanj <wahlman.j@gmail.com>
2014-06-27 01:09:19 -0500

    Create updater.bash

---
+++ update/OSX/updater.bash
@@ -0,0 +1,119 @@
+#!/bin/bash
+
+export PATH=$PATH:/usr/local/git/bin/
+
+#!/bin/bash
+cd /Applications/PlexConnect
+ 
+# fetch changes, git stores them in FETCH_HEAD
+git fetch
+ 
+# check for remote changes in origin repository
+newUpdatesAvailable=`git diff HEAD FETCH_HEAD`
+if [ "$newUpdatesAvailable" != "" ]
+then
+
+## update status
+echo 'Stopping PlexConnect...'
+
+## unload plexconnect from launchctl to avoid database errors
+cd /library/launchdaemons
+launchctl unload com.plex.plexconnect.bash.plist
+
+## find PlexConnect main path
+cd __INSTALLERPATH__
+cd ../..
+
+## change permissions of .git so update.bash can be ran without su
+chown -R __USERNAME__ .git
+
+## update status
+echo 'Updating PlexConnect...'
+
+## get update
+git pull
+
+## wait a couple seconds to allow PlexConnect to update
+sleep 3
+
+## update status
+echo 'Starting PlexConnect...'
+
+## load plexconnect into launchctl after completed update
+cd /library/launchdaemons
+launchctl load com.plex.plexconnect.bash.plist
+
+## wait a couple seconds to allow PlexConnect to load
+sleep 2
+
+## display the running status of PlexConnect
+launchctl list | grep com.plex.plexconnect.bash
+
+## Display PlexConnect log
+FILE="/Applications/PlexConnect/PlexConnect.log"
+echo "*** File - $FILE contents ***"
+cat $FILE
+
+osascript -e 'tell app "System Events" to display dialog "PlexConnect has been Updated"'
+
+else
+osascript -e 'tell app "System Events" to display dialog "No updates available"'
+fi#!/bin/bash
+
+export PATH=$PATH:/usr/local/git/bin/
+
+#!/bin/bash
+cd /Applications/PlexConnect
+ 
+# fetch changes, git stores them in FETCH_HEAD
+git fetch
+ 
+# check for remote changes in origin repository
+newUpdatesAvailable=`git diff HEAD FETCH_HEAD`
+if [ "$newUpdatesAvailable" != "" ]
+then
+
+## update status
+echo 'Stopping PlexConnect...'
+
+## unload plexconnect from launchctl to avoid database errors
+cd /library/launchdaemons
+launchctl unload com.plex.plexconnect.bash.plist
+
+## find PlexConnect main path
+cd __INSTALLERPATH__
+cd ../..
+
+## change permissions of .git so update.bash can be ran without su
+chown -R __USERNAME__ .git
+
+## update status
+echo 'Updating PlexConnect...'
+
+## get update
+git pull
+
+## wait a couple seconds to allow PlexConnect to update
+sleep 3
+
+## update status
+echo 'Starting PlexConnect...'
+
+## load plexconnect into launchctl after completed update
+cd /library/launchdaemons
+launchctl load com.plex.plexconnect.bash.plist
+
+## wait a couple seconds to allow PlexConnect to load
+sleep 2
+
+## display the running status of PlexConnect
+launchctl list | grep com.plex.plexconnect.bash
+
+## Display PlexConnect log
+FILE="/Applications/PlexConnect/PlexConnect.log"
+echo "*** File - $FILE contents ***"
+cat $FILE
+
+else
+echo 'No updates available'
+fi
