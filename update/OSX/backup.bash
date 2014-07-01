b1aca219160131b85105e06e5a918b6410b00ce8
wahlmanj <wahlman.j@gmail.com>
2014-07-01 08:16:26 -0500

    Update backup.bash

--- update/OSX/backup.bash
+++ update/OSX/backup.bash
@@ -17,8 +17,6 @@ cp /Applications/PlexConnect/settings.cfg /Applications/plexconnect_BACKUP
 
 fanartcache="${BackupPath}/fanartcache"
 if [ -d "$fanartcache" ]; then
-rm -R /Applications/plexconnect_BACKUP/fanartcache
-mkdir /Applications/plexconnect_BACKUP/fanartcache
 cp -R /Applications/PlexConnect/assets/fanartcache/* /Applications/plexconnect_BACKUP/fanartcache
 else
 mkdir /Applications/plexconnect_BACKUP/fanartcache
@@ -27,8 +25,6 @@ cp -R /Applications/PlexConnect/assets/fanartcache/* /Applications/plexconnect_B
 fi
 
 cp /Applications/PlexConnect/ATVSettings.cfg /Applications/plexconnect_BACKUP
-
-startbash.bash
 
 customicons="${BackupPath}/flow"
 if [ -d "$customicons" ]; then
@@ -41,3 +37,14 @@ cp -R /Applications/PlexConnect/assets/templates/plex/images/custom/flow/* /Appl
 cp -R /Applications/PlexConnect/assets/templates/plex/images/custom/top/* /Applications/plexconnect_BACKUP/top
 
 fi
+
+thumbnails="${BackupPath}/thumbnails"
+if [ -d "$thumbnails" ]; then
+cp -R /Applications/PlexConnect/assets/thumbnails/* /Applications/plexconnect_BACKUP/thumbnails
+else
+mkdir /Applications/plexconnect_BACKUP/thumbnails
+cp -R /Applications/PlexConnect/assets/thumbnails/* /Applications/plexconnect_BACKUP/thumbnails
+
+fi
+
+startbash.bash
