296c3f04f9930c146118c988c248a47e56581b5d
wahlmanj <wahlman.j@gmail.com>
2014-08-22 04:56:34 -0500

    Update icon.bash

--- update/OSX/icon.bash
+++ update/OSX/icon.bash
@@ -5,4 +5,4 @@ cp /Applications/PlexConnect/update/OSX/icon@1080.png /Applications/PlexConnect/
 cp /Applications/PlexConnect/update/OSX/icon@720.png /Applications/PlexConnect/assets/thumbnails
 sleep 8
 start.bash
-echo Custom plexconnect icon ready for upload
+echo 'Custom Plex icon ready to upload to aTV'
