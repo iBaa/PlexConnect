ee08e4c704e0b0c749eb1d7ffea1e44bef01c427
wahlmanj <wahlman.j@gmail.com>
2014-08-22 04:28:18 -0500

    Update icon.bash

--- update/OSX/icon.bash
+++ update/OSX/icon.bash
@@ -1,7 +1,7 @@
-stopbash.bash
+stop.bash
 cp /Applications/PlexConnect/update/OSX/WebServer.py /Applications/PlexConnect
 cp /Applications/PlexConnect/update/OSX/DNSServer.py /Applications/PlexConnect
 cp /Applications/PlexConnect/update/OSX/icon@1080.png /Applications/PlexConnect/assets/thumbnails
 cp /Applications/PlexConnect/update/OSX/icon@720.png /Applications/PlexConnect/assets/thumbnails
 sleep 8
-startbash.bash
+start.bash
