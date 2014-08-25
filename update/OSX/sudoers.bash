925e7978abd6b9bc924444cf19ffbed476381a90
wahlmanj <wahlman.j@gmail.com>
2014-08-25 09:11:27 -0500

    Update sudoers.bash

--- update/OSX/sudoers.bash
+++ update/OSX/sudoers.bash
@@ -14,13 +14,13 @@ sed -e "s/__USERNAME__/$whoami/" "/Applications/PlexConnect/update/OSX/sudoers" 
 
 fi
 
-if [ -s /Applications/PlexConnect/update/OSX/currentuser.bash
+if [ -s /Applications/PlexConnect/update/OSX/currentuser.bash ]
 
 then
 sed -e "s/__USER__/$whoami/" "/Applications/PlexConnect/update/OSX/currentuser.bash" > /Applications/PlexConnect/update/OSX/fixuser.bash
 
 else
-echo ' currentuser.bash not present'
+echo 'currentuser.bash not present'
 
 fi
 
