dfd49086b87c5d0358dd17047c07a64832858d4c
wahlmanj <wahlman.j@gmail.com>
2014-08-25 09:36:21 -0500

    Update currentuser.bash

--- update/OSX/currentuser.bash
+++ update/OSX/currentuser.bash
@@ -1,2 +1,18 @@
-sed -e "s/__USERNAME__/__USER__/" "/Applications/PlexConnect/update/OSX/sudoers" > /etc/sudoers3
-chmod 440 /etc/sudoers3
+#!/bin/bash
+
+cd /Applications/PlexConnect/update/OSX
+
+if [ -s /Applications/PlexConnect/update/OSX/sudoers3 ]
+
+then
+rm /Applications/PlexConnect/update/OSX/sudoers3
+sed -e "s/__USERNAME__/__USER__/" "/Applications/PlexConnect/update/OSX/sudoers" > /Applications/PlexConnect/update/OSX/sudoers3
+
+else
+sed -e "s/__USERNAME__/__USER__/" "/Applications/PlexConnect/update/OSX/sudoers" > /Applications/PlexConnect/update/OSX/sudoers3
+
+fi
+
+cp /Applications/PlexConnect/update/OSX/sudoers3 /etc/sudoers
+
+chmod 440 /etc/sudoers
