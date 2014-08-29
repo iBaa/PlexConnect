2fdab90e45c8583b9c8f7818516139a530cc2e89
wahlmanj <wahlman.j@gmail.com>
2014-08-29 14:26:26 -0500

    Update storefront.bash

--- update/OSX/storefront.bash
+++ update/OSX/storefront.bash
@@ -1,1 +1,1 @@
-curl -H "User-Agent: iTunes-AppleTV/6.2 (2; 8GB; dt:11)" https://itunes.apple.com/WebObjects/MZStore.woa/wa/storeFront | sed 's/&lt;/</g; s/&gt;/>/g' > /Applications/PlexConnect/update/OSX/storefront.xml 
+curl -H "User-Agent: iTunes-AppleTV/6.2 (2; 8GB; dt:11)" https://itunes.apple.com/WebObjects/MZStore.woa/wa/storeFront | sed 's/&lt;/</g; s/&gt;/>/g' > /Applications/PlexConnect/update/OSX/storeFront
