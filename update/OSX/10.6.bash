fa5f61c5b944033dd06f4b1e2cd22aa9a724e63f
wahlmanj <wahlman.j@gmail.com>
2014-09-06 13:09:28 -0500

    Update 10.6.bash

--- update/OSX/10.6.bash
+++ update/OSX/10.6.bash
@@ -4,6 +4,6 @@ cd /Applications
 rm -R OpenPlex.app
 git clone https://github.com/wahlmanj/OpenPlex.git
 cd /Applications/OpenPlex/10.6
-unzip OpenPlex.zip -d /Applications/OpenPlex/10.6
+ditto -xk OpenPlex.zip /Applications/OpenPlex/10.6
 cp -R OpenPlex.app /Applications
 rm -R /Applications/OpenPlex
