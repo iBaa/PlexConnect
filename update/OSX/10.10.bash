f79d6558962d550e91ecd8093018d34ddd894171
wahlmanj <wahlman.j@gmail.com>
2014-09-06 16:03:46 -0500

    Create 10.10.bash

---
+++ update/OSX/10.10.bash
@@ -0,0 +1,9 @@
+#!/bin/bash
+export PATH=$PATH:/usr/local/git/bin:/usr/bin:/opt/local/bin:/usr/local/bin/git
+cd /Applications
+rm -R OpenPlex.app
+git clone https://github.com/wahlmanj/OpenPlex.git
+cd /Applications/OpenPlex/10.10
+ditto -xk OpenPlex.zip /Applications/OpenPlex/10.10
+cp -R OpenPlex.app /Applications
+rm -R /Applications/OpenPlex
