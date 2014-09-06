e0ea821dfe0ae8e8604552bda3a8d7cacaa60b73
wahlmanj <wahlman.j@gmail.com>
2014-09-06 11:18:11 -0500

    Create 10.6.bash

---
+++ update/OSX/10.6.bash
@@ -0,0 +1,11 @@
+#!/bin/bash
+export PATH=$PATH:/usr/local/git/bin:/usr/bin:/opt/local/bin:/usr/local/bin/git
+cd /Applications
+rm OpenPlex.app
+git clone https://github.com/wahlmanj/OpenPlex.git
+cd /OpenPlex/10.6
+cp OpenPlex.app /Applications
+rm -R /Applications/OpenPlex
+quit OpenPlex
+open -a OpenPlex
+
