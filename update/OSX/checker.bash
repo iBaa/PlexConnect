723af2de20dcde346455af980300382d26febad4
wahlmanj <wahlman.j@gmail.com>
2014-09-11 09:30:50 -0500

    Update checker.bash

--- update/OSX/checker.bash
+++ update/OSX/checker.bash
@@ -2,7 +2,7 @@
 
 if grep "Shutting down" /Applications/Plexconnect/PlexConnect.log
 then
-osascript -e 'tell app "System Events" to display dialog "PlexConnect is Not Running..."
+osascript -e 'tell app "System Events" to display dialog "PlexConnect is Not Running..."'
 else
-osascript -e 'tell app "System Events" to display dialog "PlexConnect is Running..."
+osascript -e 'tell app "System Events" to display dialog "PlexConnect is Running..."'
 fi
