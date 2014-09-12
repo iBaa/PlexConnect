97d0f896eacf9fbf030e566d3b4628fa3aa891d0
wahlmanj <wahlman.j@gmail.com>
2014-09-12 10:09:52 -0500

    Update checker.bash

--- update/OSX/checker.bash
+++ update/OSX/checker.bash
@@ -7,9 +7,10 @@ PlexConnectPath=${PWD}
 
 basedir="${PlexConnectPath}"
 if [ -d "$basedir" ]; then
-osascript -e 'display notification "No Theme Installed..." with title "PlexConnect Status"'
-else
 echo 'fanartcache dir not found'
+else
+osascript -e 'display notification "No Theme Installed..." with title "PlexConnect Status"'
+fi
 
 if grep "Shutting down" /Applications/Plexconnect/PlexConnect.log
 then
