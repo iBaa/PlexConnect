c629b8d61340f4280b0a04a7a30d53d78a718d01
wahlmanj <wahlman.j@gmail.com>
2014-09-12 10:01:52 -0500

    Update checker.bash

--- update/OSX/checker.bash
+++ update/OSX/checker.bash
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+
+## cd to PlexConnect directory
+cd /Applications/PlexConnect
+PlexConnectPath=${PWD}
+
+basedir="${PlexConnectPath}"
+if [ -d "$basedir" ]; then
+osascript -e 'display notification "No Theme Installed..." with title "PlexConnect Status"'
+else
+echo 'fanartcache dir not found'
+
 if grep "Shutting down" /Applications/Plexconnect/PlexConnect.log
 then
 osascript -e 'display notification "PlexConnect is Not Running..." with title "PlexConnect Status"'
