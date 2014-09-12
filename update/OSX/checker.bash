2e7e1083223f4d61d104d00e83c761b2a798b0c4
wahlmanj <wahlman.j@gmail.com>
2014-09-12 12:26:32 -0500

    Update checker.bash

--- update/OSX/checker.bash
+++ update/OSX/checker.bash
@@ -12,7 +12,8 @@ then
 osascript -e 'display notification "PlexConnect is Not Running..." with title "PlexConnect Status"'
 fi
 
-if grep "Serving DNS|Serving HTTP|Serving HTTPS" /Applications/Plexconnect/PlexConnect.log
-then
+value=$( grep -ic "Serving" /Applications/Plexconnect/PlexConnect.log )
+if [ $value = 3 ]
+then 
 osascript -e 'display notification "PlexConnect is Running..." with title "PlexConnect Status"'
 fi
