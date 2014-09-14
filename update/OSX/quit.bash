89b69ea29cbb6df0b0bb6490e016f44cc03fec8c
wahlmanj <wahlman.j@gmail.com>
2014-09-14 09:54:55 -0500

    Update quit.bash

--- update/OSX/quit.bash
+++ update/OSX/quit.bash
@@ -1,4 +1,4 @@
 killall OpenPlex
 sleep 2
 open -a /Applications/OpenPlex.app
-display notification "OpenPlex updated..." with title "OpenPlex Status"
+osascript -e 'display notification "OpenPlex updated..." with title "OpenPlex Status"'
