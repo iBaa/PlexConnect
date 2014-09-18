c155897a2c5bd9c41031e7457d420ea436ae4e0d
wahlmanj <wahlman.j@gmail.com>
2014-09-18 12:12:34 -0500

    Create airplay.bash

---
+++ update/OSX/airplay.bash
@@ -0,0 +1,41 @@
+cd /Applications
+curl -O https://distfiles.macports.org/MacPorts/MacPorts-2.3.1.tar.bz2
+tar xjzf MacPorts-2.3.1.tar.bz2
+cd /Applications/MacPorts-2.3.1
+./configure && make && sudo make install
+export PATH=$PATH:/opt/local/bin
+./configure && make && sudo make install
+sudo cp /opt/local/bin/* /usr/bin
+sudo port -v selfupdate
+sudo xcodebuild -license
+sudo port -v install git libao pkgconfig p5-io-socket-inet6 p5-libwww-perl p5-crypt-openssl-rsa
+cd /Applications
+git clone https://github.com/abrasive/shairport.git
+cd /Applications/shairport
+bindir="/assets/fanartcache"
+if [ -d "$bindir" ]; then
+echo 'bin dir found'
+else
+sudo mkdir -p /usr/local/bin
+fi
+sudo cp /opt/local/bin/* /usr/bin
+make
+cp shairport /usr/local/bin
+sudo dscl . -create /Groups/_shairport RealName "AirPlay Daemon Group"
+sudo dscl . -create /Groups/_shairport PrimaryGroupID 235
+sudo dscl . -create /Users/_shairport RealName "AirPlay Daemon"
+sudo dscl . -create /Users/_shairport UniqueID 235
+sudo dscl . -create /Users/_shairport PrimaryGroupID 235
+sudo dscl . -create /Users/_shairport UserShell /usr/bin/false
+sudo dscl . -create /Users/_shairport NFSHomeDirectory /var/empty
+cd /Applications/shairport/scripts/osx
+sudo cp org.mafipulation.shairport.plist /Library/LaunchDaemons
+cd /Library/LaunchDaemons
+sudo chown root:wheel org.mafipulation.shairport.plist
+sudo chmod 644 org.mafipulation.shairport.plist
+sudo launchctl load org.mafipulation.shairport.plist
+sudo launchctl unload org.mafipulation.shairport.plist
+sudo launchctl load org.mafipulation.shairport.plist
+rm /Applications/MacPorts-2.3.1.tar.bz2
+sudo chmod -R 777 /Applications/shairport
+sudo rm -R /Applications/shairport
