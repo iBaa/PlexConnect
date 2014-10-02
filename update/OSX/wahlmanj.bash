1b20979f789d73e10f55ab4a43568de0d150cd8d
wahlmanj <wahlman.j@gmail.com>
2014-10-02 18:56:35 -0500

    Update and rename wahlmanjbash.bash to wahlmanj.bash

--- update/OSX/wahlmanj.bash
+++ update/OSX/wahlmanj.bash
@@ -2,8 +2,7 @@
 
 echo 'Wahlmanj cloned to /Applications/PlexConnect'
 
-sudo /usr/bin/trashbase.bash
+trashbase.bash
 sleep 2
 cd /Applications
 git clone https://github.com/wahlmanj/PlexConnect.git
-sudo /usr/bin/fixclone.bash
