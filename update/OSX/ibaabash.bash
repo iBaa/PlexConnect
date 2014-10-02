ce672d775896e7b2c62098699ab8c37494594c5b
wahlmanj <wahlman.j@gmail.com>
2014-10-02 18:46:56 -0500

    Update ibaabash.bash

--- update/OSX/ibaabash.bash
+++ update/OSX/ibaabash.bash
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-echo 'iBaa cloned to /Applications/PlexConnect'
-
-sudo /usr/bin/trashbase.bash
-sleep 2
-cd /Applications
-PATH=/usr/local/git/bin:/usr/bin:/opt/local/bin:/usr/local/bin/git export PATH
-git clone https://github.com/iBaa/PlexConnect.git
-mkdir -p /Applications/PlexConnect/update/OSX
-mkdir /Applications/onlytemp
-cd /Applications/onlytemp
-git clone https://github.com/wahlmanj/PlexConnect.git
-cp -R /Applications/onlytemp/PlexConnect/update/OSX/* /Applications/PlexConnect/update/OSX
+sudo /usr/bin/trashbase.bash;
+sleep 2;
+PATH=/usr/local/git/bin:/usr/bin:/opt/local/bin:/usr/local/bin/git export PATH;
+cd /Applications;
+git clone https://github.com/iBaa/PlexConnect.git;
+mkdir -p /Applications/PlexConnect/update/OSX;
+mkdir /Applications/onlytemp;
+cd /Applications/onlytemp;
+git clone https://github.com/wahlmanj/PlexConnect.git;
+cp -R /Applications/onlytemp/PlexConnect/update/OSX/* /Applications/PlexConnect/update/OSX;
 rm -R /Applications/onlytemp
+
+echo 'iBaa cloned to /Applications/PlexConnect'
