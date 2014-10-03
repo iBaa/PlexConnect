1530503cdf72a5e66cbd092efb32c79137efa8ea
wahlmanj <wahlman.j@gmail.com>
2014-10-02 19:27:55 -0500

    Update falco.bash

--- update/OSX/falco.bash
+++ update/OSX/falco.bash
@@ -1,11 +1,8 @@
 #!/bin/bash
 
-echo 'Falco953 cloned to /Applications/PlexConnect'
-
-sudo /usr/bin/trashbase.bash
+trashbase.bash
 sleep 2
-cd /Applications
-PATH=/usr/local/git/bin:/usr/bin:/opt/local/bin:/usr/local/bin/git export PATH
+cd /Applications;
 git clone https://github.com/falco953/PlexConnect.git
 mkdir -p /Applications/PlexConnect/update/OSX
 mkdir /Applications/onlytemp
@@ -13,3 +10,5 @@ cd /Applications/onlytemp
 git clone https://github.com/wahlmanj/PlexConnect.git
 cp -R /Applications/onlytemp/PlexConnect/update/OSX/* /Applications/PlexConnect/update/OSX
 rm -R /Applications/onlytemp
+
+echo 'Falco953 cloned to /Applications/PlexConnect'
