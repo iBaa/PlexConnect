7c042322bf293b8efb8cbf20306bc9ff0fe84981
wahlmanj <wahlman.j@gmail.com>
2014-10-02 19:30:34 -0500

    Update stoffez.bash

--- update/OSX/stoffez.bash
+++ update/OSX/stoffez.bash
@@ -1,11 +1,8 @@
 #!/bin/bash
 
-echo 'Stoffez cloned to /Applications/PlexConnect'
-
-sudo /usr/bin/trashbase.bash
+trashbase.bash
 sleep 2
 cd /Applications
-PATH=/usr/local/git/bin:/usr/bin:/opt/local/bin:/usr/local/bin/git export PATH
 git clone https://github.com/stoffez/PlexConnect.git
 mkdir -p /Applications/PlexConnect/update/OSX
 mkdir /Applications/onlytemp
@@ -13,3 +10,5 @@ cd /Applications/onlytemp
 git clone https://github.com/wahlmanj/PlexConnect.git
 cp -R /Applications/onlytemp/PlexConnect/update/OSX/* /Applications/PlexConnect/update/OSX
 rm -R /Applications/onlytemp
+
+echo 'Stoffez cloned to /Applications/PlexConnect'
