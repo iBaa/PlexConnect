26cc900f3356246d89591b921eb83dd8310f75d3
wahlmanj <wahlman.j@gmail.com>
2014-10-02 19:26:21 -0500

    Update cyberghost.bash

--- update/OSX/cyberghost.bash
+++ update/OSX/cyberghost.bash
@@ -1,11 +1,8 @@
 #!/bin/bash
 
-echo 'CyberGhost84 cloned to /Applications/PlexConnect'
-
-sudo /usr/bin/trashbase.bash
+trashbase.bash
 sleep 2
 cd /Applications
-PATH=/usr/local/git/bin:/usr/bin:/opt/local/bin:/usr/local/bin/git export PATH
 git clone https://github.com/CyberGhost84/PlexConnect.git
 mkdir -p /Applications/PlexConnect/update/OSX
 mkdir /Applications/onlytemp
@@ -13,3 +10,5 @@ cd /Applications/onlytemp
 git clone https://github.com/wahlmanj/PlexConnect.git
 cp -R /Applications/onlytemp/PlexConnect/update/OSX/* /Applications/PlexConnect/update/OSX
 rm -R /Applications/onlytemp
+
+echo 'CyberGhost84 cloned to /Applications/PlexConnect'
