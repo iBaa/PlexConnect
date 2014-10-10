13a95cafa21a3aa1e5f647fc84dd21806db04463
iBaa <bernhard.baaser@gmx.de>
2014-10-10 18:36:49 +0200

    "Playlists" now support Audio as well...

--- XMLConverter.py
+++ XMLConverter.py
@@ -355,7 +355,16 @@ def XML_PMS2aTV(PMS_address, path, options):
 
     elif cmd == 'DirectoryWithPreviewActors':
         XMLtemplate = 'DirectoryWithPreviewActors.xml'
-            
+    
+    elif cmd=='Playlists':
+        XMLtemplate = 'Playlists.xml'
+    
+    elif cmd=='Playlist_Video':
+        XMLtemplate = 'Playlist_Video.xml'
+    
+    elif cmd=='Playlist_Audio':
+        XMLtemplate = 'Playlist_Audio.xml'
+    
     elif cmd=='Settings':
         XMLtemplate = 'Settings.xml'
         path = ''  # clear path - we don't need PMS-XML
@@ -523,11 +532,8 @@ def XML_PMS2aTV(PMS_address, path, options):
             # Photo listing / directory
             XMLtemplate = 'Photo_Directories.xml'
     
-    elif cmd=='Playlists':
-        XMLtemplate = 'Playlists.xml'
-    
-    elif cmd=='Playlist_Video':
-        XMLtemplate = 'Playlist_Video.xml'
+    elif PMSroot.get('viewGroup','')=='track':
+        XMLtemplate = "Music_Track.xml"
     
     else:
         XMLtemplate = 'Directory.xml'
