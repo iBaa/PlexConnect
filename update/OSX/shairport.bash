6485c9ccad36f0b59b55edb66e5bbc46e245b923
wahlmanj <wahlman.j@gmail.com>
2014-10-21 00:23:29 -0500

    Update shairport.bash

--- update/OSX/shairport.bash
+++ update/OSX/shairport.bash
@@ -1,11 +1,27 @@
+cd /Applications
+curl -O https://distfiles.macports.org/MacPorts/MacPorts-2.3.1.tar.bz2
+tar xf MacPorts-2.3.1.tar.bz2
+cd /Applications/MacPorts-2.3.1
+export PATH=$PATH:/usr/local/git/bin:/usr/bin:/opt/local/bin:/usr/local/bin/git:/usr/local/bin
+./configure && make && sudo make install
+sudo cp /opt/local/bin/* /usr/bin
+sudo port -v selfupdate
 sudo xcodebuild -license
-sudo port install git libao pkgconfig p5-io-socket-inet6 p5-libwww-perl p5-crypt-openssl-rsa
+sudo port -v install git libao pkgconfig p5-io-socket-inet6 p5-libwww-perl p5-crypt-openssl-rsa
 cd /Applications
 git clone https://github.com/abrasive/shairport.git
 cd /Applications/shairport
+bindir="/usr/local/bin"
+if [ -d "$bindir" ]; then
+echo 'bin dir found'
+else
+sudo mkdir -p /usr/local/bin
+fi
+sudo cp /opt/local/bin/* /usr/bin
+export PATH=$PATH:/usr/local/git/bin:/usr/bin:/opt/local/bin:/usr/local/bin/git:/usr/local/bin
 make
+cd /Applications/shairport
 sudo cp shairport /usr/local/bin
-sudo chmod +x /usr/local/bin/shairport
 sudo dscl . -create /Groups/_shairport RealName "AirPlay Daemon Group"
 sudo dscl . -create /Groups/_shairport PrimaryGroupID 235
 sudo dscl . -create /Users/_shairport RealName "AirPlay Daemon"
@@ -19,3 +35,11 @@ cd /Library/LaunchDaemons
 sudo chown root:wheel org.mafipulation.shairport.plist
 sudo chmod 644 org.mafipulation.shairport.plist
 sudo launchctl load org.mafipulation.shairport.plist
+sudo launchctl unload org.mafipulation.shairport.plist
+sudo launchctl load org.mafipulation.shairport.plist
+sudo chmod -R 777 /Applications/shairport
+sudo chmod -R 777 /Applications/MacPorts-2.3.1.tar.bz2
+sudo chmod -R 777 /Applications/MacPorts-2.3.1
+sudo rm -R /Applications/shairport
+rm /Applications/MacPorts-2.3.1.tar.bz2
+rm -R /Applications/MacPorts-2.3.1
