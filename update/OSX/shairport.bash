55f45fbe2b2d99da0402c8d1e34a588e7da57094
wahlmanj <wahlman.j@gmail.com>
2014-10-22 04:00:07 -0500

    Update shairport.bash

--- update/OSX/shairport.bash
+++ update/OSX/shairport.bash
@@ -1,23 +1,24 @@
-cd /Applications
-curl -O https://distfiles.macports.org/MacPorts/MacPorts-2.3.2.tar.bz2
-tar xf MacPorts-2.3.2.tar.bz2
-cd /Applications/MacPorts-2.3.2
-export PATH=$PATH:/usr/local/git/bin:/usr/bin:/opt/local/bin:/usr/local/bin/git:/usr/local/bin
-./configure && make && sudo make install
-sudo cp /opt/local/bin/* /usr/bin
-sudo port -v selfupdate
+# Check for brew and install it
+if [ ! -f "/usr/local/bin/brew" ]; then
+  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  brew prune
+fi
+
+brew tap homebrew/headonly
+brew install shairport --HEAD --with-libao
+
 sudo xcodebuild -license
-sudo port -v install git libao pkgconfig p5-io-socket-inet6 p5-libwww-perl p5-crypt-openssl-rsa
-cd /Applications
-git clone https://github.com/abrasive/shairport.git
-cd /Applications/shairport
+
 bindir="/usr/local/bin"
 if [ -d "$bindir" ]; then
 echo 'bin dir found'
 else
 sudo mkdir -p /usr/local/bin
 fi
-sudo cp /opt/local/bin/* /usr/bin
+
+cd /Applications
+git clone https://github.com/abrasive/shairport.git
+cd /Applications/shairport
 export PATH=$PATH:/usr/local/git/bin:/usr/bin:/opt/local/bin:/usr/local/bin/git:/usr/local/bin
 make
 cd /Applications/shairport
@@ -37,9 +38,3 @@ sudo chmod 644 org.mafipulation.shairport.plist
 sudo launchctl load org.mafipulation.shairport.plist
 sudo launchctl unload org.mafipulation.shairport.plist
 sudo launchctl load org.mafipulation.shairport.plist
-sudo chmod -R 777 /Applications/shairport
-sudo chmod -R 777 /Applications/MacPorts-2.3.1.tar.bz2
-sudo chmod -R 777 /Applications/MacPorts-2.3.1
-sudo rm -R /Applications/shairport
-rm /Applications/MacPorts-2.3.2.tar.bz2
-rm -R /Applications/MacPorts-2.3.2
