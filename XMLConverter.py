101bb3a8d901038070a4d617922775af0260e5ed
roidy <roidy.parnell@ntlworld.com>
2014-10-22 19:40:33 +0100

    Respond to the aTV when requesting to save settings.

--- XMLConverter.py
+++ XMLConverter.py
@@ -403,6 +403,7 @@ def XML_PMS2aTV(PMS_address, path, options):
     elif cmd=='SaveSettings':
         g_ATVSettings.saveSettings();
         path = ''  # clear path - we don't need PMS-XML
+        return XML_Error('PlexConnect', 'SaveSettings!')  # not an error - but aTV won't care anyways.
         
     elif cmd.startswith('SettingsToggle:'):
         opt = cmd[len('SettingsToggle:'):]  # cut command:
