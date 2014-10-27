8a293ecb876f383baed6f4c65ec5638fa7f89f76
wahlmanj <wahlman.j@gmail.com>
2014-10-26 21:07:34 -0500

    Speed up OpenPlex

--- support/OSX/createcert.bash
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-## cd to PlexConnect directory
-cd "$( cd "$( dirname "$0" )" && pwd )"/../..
-
-## Trailers - hostname is trailers.apple.com
-## certificate good for 10 years
-openssl req -new -nodes -newkey rsa:2048 -outform pem -out ./assets/certificates/trailers.cer -keyout ./assets/certificates/trailers.key -x509 -days 3650 -subj "/C=US/CN=trailers.apple.com"
-cat ./assets/certificates/trailers.cer ./assets/certificates/trailers.key >> ./assets/certificates/trailers.pem
