59252b51a39b65a19966d9e0b292836776f1f3af
Tunghsiao Liu <t@sparanoid.com>
2014-11-27 14:18:28 +0800

    Fix reload script for OS X

--- support/OSX/restart.bash
+++ support/OSX/restart.bash
@@ -6,11 +6,11 @@ echo 'Restarting PlexConnect...'
 ## unload plist
 launchctl unload /Library/LaunchDaemons/com.plex.plexconnect.bash.plist
 
-## unload plist
-launchctl load /Library/LaunchDaemons/com.plex.plexconnect.bash.plist
-
 ## wait a second to allow PlexConnect to load
 sleep 1
+
+## reload plist
+launchctl load /Library/LaunchDaemons/com.plex.plexconnect.bash.plist
 
 ## display the running status of PlexConnect
 #./PlexConnect_daemon.bash status
