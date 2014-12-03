375dc796004a96c310e3d5736a5173f31c993604
wahlmanj <wahlman.j@gmail.com>
2014-12-03 15:11:00 -0600

    Fix lowercase "s" in WSJ

--- update/OSX/createwsj.bash
+++ update/OSX/createwsj.bash
@@ -5,8 +5,8 @@ cd __INSTALLERPATH__
 cd ../..
 PlexConnectPath=${PWD}
 
-sed -i '' 's/trailers.apple.com/secure.marketwatch.com/g' settings.cfg
-sed -i '' 's/www.icloud.com/secure.marketwatch.com/g' settings.cfg
+sed -i '' 's/trailers.apple.com/secure.marketwatch.com/g' Settings.cfg
+sed -i '' 's/www.icloud.com/secure.marketwatch.com/g' Settings.cfg
 
 ## check SSL certificate
 file="${PlexConnectPath}/assets/certificates/trailers.pem"
@@ -22,8 +22,8 @@ else
 openssl req -new -nodes -newkey rsa:2048 -out ./assets/certificates/trailers.pem -keyout ./assets/certificates/trailers.key -x509 -days 3650 -subj "/C=US/CN=secure.marketwatch.com"
 openssl x509 -in ./assets/certificates/trailers.pem -outform der -out ./assets/certificates/trailers.cer && cat ./assets/certificates/trailers.key >> ./assets/certificates/trailers.pem
 
-sed -i '' 's/trailers.apple.com/secure.marketwatch.com/g' settings.cfg
-sed -i '' 's/www.icloud.com/secure.marketwatch.com/g' settings.cfg
+sed -i '' 's/trailers.apple.com/secure.marketwatch.com/g' Settings.cfg
+sed -i '' 's/www.icloud.com/secure.marketwatch.com/g' Settings.cfg
 
 echo 'Generating WSJ certs'
 echo 'Settings.cfg changed to hijack secure.marketwatch.com'
