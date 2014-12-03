6e8a5857cccea014a0530484d4fb6fe9609e656f
wahlmanj <wahlman.j@gmail.com>
2014-12-03 15:09:19 -0600

    Fix lowercase "s" in trailers

--- update/OSX/createcert.bash
+++ update/OSX/createcert.bash
@@ -6,8 +6,8 @@ cd ../..
 PlexConnectPath=${PWD}
 
 
-sed -i '' 's/www.icloud.com/trailers.apple.com/g' settings.cfg
-sed -i '' 's/secure.marketwatch.com/trailers.apple.com/g' settings.cfg
+sed -i '' 's/www.icloud.com/trailers.apple.com/g' Settings.cfg
+sed -i '' 's/secure.marketwatch.com/trailers.apple.com/g' Settings.cfg
 
 ## check SSL certificate
 file="${PlexConnectPath}/assets/certificates/trailers.pem"
@@ -22,8 +22,8 @@ else
 openssl req -new -nodes -newkey rsa:2048 -out ./assets/certificates/trailers.pem -keyout ./assets/certificates/trailers.key -x509 -days 3650 -subj "/C=US/CN=trailers.apple.com"
 openssl x509 -in ./assets/certificates/trailers.pem -outform der -out ./assets/certificates/trailers.cer && cat ./assets/certificates/trailers.key >> ./assets/certificates/trailers.pem
 
-sed -i '' 's/www.icloud.com/trailers.apple.com/g' settings.cfg
-sed -i '' 's/secure.marketwatch.com/trailers.apple.com/g' settings.cfg
+sed -i '' 's/www.icloud.com/trailers.apple.com/g' Settings.cfg
+sed -i '' 's/secure.marketwatch.com/trailers.apple.com/g' Settings.cfg
 
 echo 'Generating Trailers certs'
 echo 'Settings.cfg changed to hijack trailers.apple.com'
