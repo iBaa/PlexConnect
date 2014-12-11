4503e6fcdd47da230d94e59508dadcbda5ae5aba
wahlmanj <wahlman.j@gmail.com>
2014-12-11 03:35:27 -0600

    Update purgeapp.bash

--- update/OSX/purgeapp.bash
+++ update/OSX/purgeapp.bash
@@ -4,30 +4,3 @@ if [ -s OpenPlex.app ]
 then
 rm -R OpenPlex.app
 fi
-
-if [ -s updater.app ]
-then
-rm -R updater.app
-fi
-
-cd /Applications/OpenPlex/updater
-
-if [ -s updater.app ]
-then
-rm -R updater.app
-fi
-
-cd /Applications/OpenPlex/10.6
-
-if [ -s OpenPlex.app ]
-then
-rm -R OpenPlex.app
-fi
-
-cd /Applications/OpenPlex/10.7
-
-if [ -s OpenPlex.app ]
-then
-rm -R OpenPlex.app
-fi
-
