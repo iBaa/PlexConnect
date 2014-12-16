9e92b7af8ea329c00a16a225f3456d120ccd1231
wahlmanj <wahlman.j@gmail.com>
2014-12-16 04:40:55 -0600

    Update clt.bash

--- update/OSX/clt.bash
+++ update/OSX/clt.bash
@@ -10,9 +10,9 @@ if [ ! -f "$TOOLS" ]; then
   elif [ "$OSX_VERS" -eq 8 ]; then
   	  DMGURL=http://devimages.apple.com/downloads/xcode/command_line_tools_for_xcode_os_x_mountain_lion_april_2013.dmg
   elif [ "$OSX_VERS" -eq 9 ]; then
-	  DMGURL=http://adcdownload.apple.com/Developer_Tools/command_line_tools_os_x_mavericks_for_xcode__late_october_2013/command_line_tools_os_x_mavericks_for_xcode__late_october_2013.dmg
+	  DMGURL=http://qd.baidupcs.com/file/b19bb382d758d995662d21dc149484c3?bkt=p2-nj-897&fid=422877698-250528-46527744964142&time=1418726215&sign=FDTAXERLB-DCb740ccc5511e5e8fedcff06b081203-kfAjPZ3DYzb1sQgqexyHIKABjIc%3D&to=qb&fm=Qin,B,U,ny&newver=1&newfm=1&flow_ver=3&sl=81723464&expires=1418728119&rt=sh&r=448629972&mlogid=1246248485&sh=1&vuk=-&vbdid=844524738&fin=command_line_tools_for_osx_mavericks_june_2014.dmg
   elif [ "$OSX_VERS" -eq 10 ]; then
-	  DMGURL=https://www.dropbox.com/sh/4ebx2bmx7ca6c0h/AACP3yeFDdSbjnpBriOdtSOia/10.10/command_line_tools_for_osx_10_10_june_2014.dmg?dl=1
+	  DMGURL=http://qd.baidupcs.com/file/b19bb382d758d995662d21dc149484c3?bkt=p2-nj-897&fid=422877698-250528-46527744964142&time=1418726215&sign=FDTAXERLB-DCb740ccc5511e5e8fedcff06b081203-kfAjPZ3DYzb1sQgqexyHIKABjIc%3D&to=qb&fm=Qin,B,U,ny&newver=1&newfm=1&flow_ver=3&sl=81723464&expires=1418728119&rt=sh&r=448629972&mlogid=1246248485&sh=1&vuk=-&vbdid=844524738&fin=command_line_tools_for_osx_10_10_june_2014.dmg
   fi
   curl "$DMGURL" -o "$TOOLS"
 fi
