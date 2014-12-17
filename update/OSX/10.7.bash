a0e9a9f98952ebca98bce38c2c6af34469f47987
wahlmanj <wahlman.j@gmail.com>
2014-12-17 17:33:43 -0600

    Update 10.7.bash

--- update/OSX/10.7.bash
+++ update/OSX/10.7.bash
@@ -1,5 +1,6 @@
 #!/bin/bash
-export PATH=$PATH:/usr/local/git/bin:/usr/bin:/opt/local/bin:/usr/local/bin/git
+
+export PATH=/usr/local/git/bin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:$PATH
 cd /Applications
 rm -R OpenPlex.app
 git clone https://github.com/wahlmanj/OpenPlex.git
