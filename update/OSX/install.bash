816c9742256ba6486faa281e2152a058f20807f3
wahlmanj <wahlman.j@gmail.com>
2014-12-19 08:26:28 -0600

    Update install.bash

--- update/OSX/install.bash
+++ update/OSX/install.bash
@@ -392,7 +392,6 @@ cp sudoers.bash /usr/bin
 cp restorecerts.bash /usr/bin
 cp restore.bash /usr/bin
 cp backup.bash /usr/bin
-cp dark-mode /usr/bin
 cp icon.bash /usr/bin
 cp 10.6.bash /usr/bin
 cp 10.7.bash /usr/bin
