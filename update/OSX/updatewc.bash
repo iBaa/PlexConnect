deb26ce10040648007cf22d9c83ab63a38967038
wahlmanj <wahlman.j@gmail.com>
2014-12-20 22:19:31 -0600

    Update updatewc.bash

--- update/OSX/updatewc.bash
+++ update/OSX/updatewc.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export PATH=/usr/local/git/bin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:$PATH
+export PATH=/usr/local/git/bin$PATH
 mkdir /Applications/onlytemp
 cd /Applications/onlytemp
 git clone https://github.com/wahlmanj/PlexConnect.git
