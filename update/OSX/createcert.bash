fc1a3990ef93100dd3889c2b27e50f2db5d8a2c2
wahlmanj <wahlman.j@gmail.com>
2014-12-23 02:21:34 -0600

    Update createcert.bash

--- update/OSX/createcert.bash
+++ update/OSX/createcert.bash
@@ -9,12 +9,6 @@ createplistbash.bash
 stopbash.bash
 startbash.bash
 
-## Wait till PlexConnect saves settings
-sleep 1
-
-sed -i '' 's/www.icloud.com/trailers.apple.com/g' Settings.cfg
-sed -i '' 's/secure.marketwatch.com/trailers.apple.com/g' Settings.cfg
-
 ## check SSL certificate
 file="${PlexConnectPath}/assets/certificates/trailers.pem"
 if [ -f "$file" ]; then
@@ -28,14 +22,14 @@ else
 openssl req -new -nodes -newkey rsa:2048 -out ./assets/certificates/trailers.pem -keyout ./assets/certificates/trailers.key -x509 -days 3650 -subj "/C=US/CN=trailers.apple.com"
 openssl x509 -in ./assets/certificates/trailers.pem -outform der -out ./assets/certificates/trailers.cer && cat ./assets/certificates/trailers.key >> ./assets/certificates/trailers.pem
 
-sed -i '' 's/www.icloud.com/trailers.apple.com/g' Settings.cfg
-sed -i '' 's/secure.marketwatch.com/trailers.apple.com/g' Settings.cfg
-
 echo 'Generating Trailers certs'
 echo 'Settings.cfg changed to hijack trailers.apple.com'
 echo 'Upload profile to ATV using this url http://trailers.apple.com/trailers.cer'
 
 fi
+
+sed -i '' 's/www.icloud.com/trailers.apple.com/g' Settings.cfg
+sed -i '' 's/secure.marketwatch.com/trailers.apple.com/g' Settings.cfg
 
 restartbash.bash
 
