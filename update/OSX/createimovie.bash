54e88dc2f3492bc5448065ec8f601a1be27f00d3
wahlmanj <wahlman.j@gmail.com>
2014-12-23 02:22:15 -0600

    Update createimovie.bash

--- update/OSX/createimovie.bash
+++ update/OSX/createimovie.bash
@@ -9,12 +9,6 @@ createplistbash.bash
 stopbash.bash
 startbash.bash
 
-## Wait till PlexConnect saves settings
-sleep 1
-
-sed -i '' 's/trailers.apple.com/www.icloud.com/g' Settings.cfg
-sed -i '' 's/secure.marketwatch.com/www.icloud.com/g' Settings.cfg
-
 ## check SSL certificate
 file="${PlexConnectPath}/assets/certificates/trailers.pem"
 if [ -f "$file" ]; then
@@ -28,14 +22,14 @@ else
 openssl req -new -nodes -newkey rsa:2048 -out ./assets/certificates/trailers.pem -keyout ./assets/certificates/trailers.key -x509 -days 3650 -subj "/C=US/CN=www.icloud.com"
 openssl x509 -in ./assets/certificates/trailers.pem -outform der -out ./assets/certificates/trailers.cer && cat ./assets/certificates/trailers.key >> ./assets/certificates/trailers.pem
 
-sed -i '' 's/trailers.apple.com/www.icloud.com/g' Settings.cfg
-sed -i '' 's/secure.marketwatch.com/www.icloud.com/g' Settings.cfg
-
 echo 'Generating Imovie certs'
 echo 'Settings.cfg changed to hijack www.icloud.com'
 echo 'Upload profile to ATV using this url http://www.icloud.com/trailers.cer'
 
 fi
+
+sed -i '' 's/trailers.apple.com/www.icloud.com/g' Settings.cfg
+sed -i '' 's/secure.marketwatch.com/www.icloud.com/g' Settings.cfg
 
 restartbash.bash
 
