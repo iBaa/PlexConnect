a008e60a350a8bd9abecca6f1633ea33067d45e7
wahlmanj <wahlman.j@gmail.com>
2014-12-23 15:46:40 -0600

    Update start.bash

--- update/OSX/start.bash
+++ update/OSX/start.bash
@@ -11,7 +11,7 @@ launchctl load com.plex.plexconnect.bash.plist
 launchctl list | grep com.plex.plexconnect.bash
 
 ## Allow log to refresh
-Sleep 2
+Sleep 4
 
 if [ ! -f /Applications/PlexConnect/PlexConnect.log ]
 then
