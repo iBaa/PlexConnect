20c955136bdf3a52add68f392ca42ea7dbc1cdd2
wahlmanj <wahlman.j@gmail.com>
2014-12-31 17:07:02 -0600

    Update fixbash.bash

--- update/OSX/fixbash.bash
+++ update/OSX/fixbash.bash
@@ -1,6 +1,12 @@
+#!/bin/bash
+
+if [ -s /Applications/PlexConnect/update/OSX ]
+then
+cd /Applications/PlexConnect/update/OSX
 ed -s PlexConnect.bash << EOF
 i
 export PATH=/usr/local/bin:$PATH
-. 
+.
 wq
 EOF
+fi
