cf0c709ea27095c89c043c2f097961dff2d78ce3
jdrydn <james@jdrydn.com>
2015-02-20 17:58:54 +0000

    Added the ability to override the Trailers icon with a PlexConnect one.
    For reference & icon source please see this thread:
    https://forums.plex.tv/index.php/topic/95309-trailers-icon-to-plex-icon

--- Settings.py
+++ Settings.py
@@ -29,6 +29,7 @@ g_settings = [
     ('port_dnsserver'  , ('53', '[0-9]{1,5}')),
     ('ip_dnsmaster'    , ('8.8.8.8', '([0-9]{1,3}\.){3}[0-9]{1,3}')),
     ('prevent_atv_update'           , ('True', '((True)|(False))')),
+    ('intercept_atv_icon', ('True', '((True)|(False))')),
     \
     ('enable_plexconnect_autodetect', ('True', '((True)|(False))')),
     ('ip_plexconnect'  , ('0.0.0.0', '([0-9]{1,3}\.){3}[0-9]{1,3}')),
--- WebServer.py
+++ WebServer.py
@@ -152,6 +152,27 @@ class MyHandler(BaseHTTPRequestHandler):
             dprint(__name__, 2, "PlexConnect options:\n{0}", options)
             dprint(__name__, 2, "additional arguments:\n{0}", query)
             
+            if "a1.phobos.apple.com" in self.headers['Host']:
+                resource = self.headers['Host']+self.path
+                try:
+                    if self.path.endswith("movie-trailers@1080.png"):
+                        resource = './assets/icons/icon@1080.png'
+                        dprint(__name__, 1, "serving "+self.headers['Host']+self.path+" with "+resource)
+                        r = open(resource, "rb")
+                    elif self.path.endswith("movie-trailers@720.png"):
+                        resource = './assets/icons/icon@720.png'
+                        dprint(__name__, 1, "serving "+self.headers['Host']+self.path+" with "+resource)
+                        r = open(resource, "rb")
+                    else:
+                        r = urllib.urlopen('http://'+resource)
+                except:
+                    dprint(__name__, 0, "Failed to access resource: {0}", resource)
+                    return
+                # dprint(__name__, 1, "serving "+self.headers['Host']+self.path+" with "+resource)
+                self.sendResponse(r.read(), 'image/png', False)
+                r.close()
+                return
+            
             if 'User-Agent' in self.headers and \
                'AppleTV' in self.headers['User-Agent']:
                 
