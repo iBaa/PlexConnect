ecb050eb74577369af575eaf3441f6e202e0e7de
iBaa <bernhard.baaser@gmx.de>
2015-11-23 20:22:20 +0100

    Add shared channels view - #404 plus some cosmetics - thanks vin047!

--- XMLConverter.py
+++ XMLConverter.py
@@ -1100,7 +1100,7 @@ class CCommandCollection(CCommandHelper):
         addOpt, leftover = self.getParam(src, leftover)
         
         # compare PMS_mark in PlexAPI/getXMLFromMultiplePMS()
-        PMS_mark = '/PMS(' + PlexAPI.getPMSProperty(self.ATV_udid, self.PMS_uuid, 'address') + ')'
+        PMS_mark = '/PMS(' + self.PMS_address + ')'
         
         # overwrite with URL embedded PMS address
         cmd_start = key.find('PMS(')
