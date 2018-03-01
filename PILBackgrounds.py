b1667abb2799d54c17117031887b0448a7ef46ce
Fish2 <Ghill13.fish2@gmail.com>
2018-03-01 13:36:12 +0000

    Add new fanart_quality function

--- PILBackgrounds.py
+++ PILBackgrounds.py
@@ -83,7 +83,10 @@ def generate(PMS_uuid, url, authtoken, resolution, blurRadius):
         dprint(__name__,1 , "aTV Height: {0}, {1}", width, height)
         
         if bgHeight != height:
-            background = background.resize((width, height), Image.ANTIALIAS)
+            if CSettings.getSetting('fanart_quality')=='High':
+                background = background.resize((width, height), Image.ANTIALIAS)
+            else:
+                background = background.resize((width, height), Image.NEAREST)
             dprint(__name__,1 , "Resizing background")
         
         if blurRadius != 0:
