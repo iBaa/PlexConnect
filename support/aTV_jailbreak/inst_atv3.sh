67b61b51b8134116cd5c4d809e9ced59e1f5034c
moodyblue <hugo.castelobranco@gmail.com>
2026-05-13 01:40:28 +0100

    Update inst_atv3.sh
    Use kortv by default

--- support/aTV_jailbreak/inst_atv3.sh
+++ support/aTV_jailbreak/inst_atv3.sh
@@ -2,9 +2,9 @@
 dpkg -i python_2.7.6-3_iphoneos-arm.deb
 rm python_2.7.6-3_iphoneos-arm.deb
 cp Settings_atv3.cfg /Applications/PlexConnect/Settings.cfg
-cp iMovie.* /Applications/PlexConnect/assets/certificates/
-cp /Applications/PlexConnect/assets/icons/icon\@720.png /private/var/mobile/Library/Caches/AppleTV/MainMenu/iMovieNewAuth\@720.png
-cp /Applications/PlexConnect/assets/icons/icon\@1080.png /private/var/mobile/Library/Caches/AppleTV/MainMenu/iMovieNewAuth\@1080.png
+cp kortv.* /Applications/PlexConnect/assets/certificates/
+# cp /Applications/PlexConnect/assets/icons/icon\@720.png /private/var/mobile/Library/Caches/AppleTV/MainMenu/iMovieNewAuth\@720.png
+# cp /Applications/PlexConnect/assets/icons/icon\@1080.png /private/var/mobile/Library/Caches/AppleTV/MainMenu/iMovieNewAuth\@1080.png
 echo '## PlexConnect managed app' >> /etc/hosts
 echo '127.0.0.1 www.icloud.com' >> /etc/hosts
 echo '## Avoid firmware update' >> /etc/hosts
